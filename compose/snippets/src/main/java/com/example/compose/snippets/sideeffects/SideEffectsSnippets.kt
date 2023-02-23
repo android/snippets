84b5b1705442cf7874e83ecae2c15e499e64ecd6
Chris Arriola <carriola@google.com>
2023-02-23 11:04:24 -0800

    Add snippets for /side-effects page. (#58)
    * Add snippets for /side-effects page.
    
    * Apply Spotless
    
    ---------
    
    Co-authored-by: arriolac <arriolac@users.noreply.github.com>

---
+++ compose/snippets/src/main/java/com/example/compose/snippets/sideeffects/SideEffectsSnippets.kt
@@ -0,0 +1,264 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.compose.snippets.sideeffects
+
+import android.media.Image
+import androidx.compose.foundation.gestures.ModifierLocalScrollableContainerProvider.value
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
+import com.example.compose.snippets.interop.FirebaseAnalytics
+import com.example.compose.snippets.interop.User
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+// [START android_compose_side_effects_launchedeffect]
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyScreen(
+    state: UiState<List<Movie>>,
+    snackbarHostState: SnackbarHostState
+) {
+
+    // If the UI state contains an error, show snackbar
+    if (state.hasError) {
+
+        // `LaunchedEffect` will cancel and re-launch if
+        // `scaffoldState.snackbarHostState` changes
+        LaunchedEffect(snackbarHostState) {
+            // Show snackbar using a coroutine, when the coroutine is cancelled the
+            // snackbar will automatically dismiss. This coroutine will cancel whenever
+            // `state.hasError` is false, and only start when `state.hasError` is true
+            // (due to the above if-check), or if `scaffoldState.snackbarHostState` changes.
+            snackbarHostState.showSnackbar(
+                message = "Error message",
+                actionLabel = "Retry message"
+            )
+        }
+    }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { contentPadding ->
+        // [START_EXCLUDE]
+        Box(Modifier.padding(contentPadding))
+        // [END_EXCLUDE]
+    }
+}
+// [START_EXCLUDE silent]
+class Movie
+class UiState<T> {
+    val hasError = true
+}
+// [END_EXCLUDE]
+// [END android_compose_side_effects_launchedeffect]
+
+// [START android_compose_side_effects_remembercoroutinescope]
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MoviesScreen(snackbarHostState: SnackbarHostState) {
+
+    // Creates a CoroutineScope bound to the MoviesScreen's lifecycle
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { contentPadding ->
+        Column(Modifier.padding(contentPadding)) {
+            Button(
+                onClick = {
+                    // Create a new coroutine in the event handler to show a snackbar
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Something happened!")
+                    }
+                }
+            ) {
+                Text("Press me")
+            }
+        }
+    }
+}
+// [END android_compose_side_effects_remembercoroutinescope]
+
+// [START android_compose_side_effects_rememberupdatedstate]
+@Composable
+fun LandingScreen(onTimeout: () -> Unit) {
+
+    // This will always refer to the latest onTimeout function that
+    // LandingScreen was recomposed with
+    val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+    // Create an effect that matches the lifecycle of LandingScreen.
+    // If LandingScreen recomposes, the delay shouldn't start again.
+    LaunchedEffect(true) {
+        delay(SplashWaitTimeMillis)
+        currentOnTimeout()
+    }
+
+    /* Landing screen content */
+}
+// [START_EXCLUDE silent]
+private const val SplashWaitTimeMillis = 1000L
+// [END_EXCLUDE]
+// [END android_compose_side_effects_rememberupdatedstate]
+
+// [START android_compose_side_effects_disposableeffect]
+@Composable
+fun HomeScreen(
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
+    onStart: () -> Unit, // Send the 'started' analytics event
+    onStop: () -> Unit // Send the 'stopped' analytics event
+) {
+    // Safely update the current lambdas when a new one is provided
+    val currentOnStart by rememberUpdatedState(onStart)
+    val currentOnStop by rememberUpdatedState(onStop)
+
+    // If `lifecycleOwner` changes, dispose and reset the effect
+    DisposableEffect(lifecycleOwner) {
+        // Create an observer that triggers our remembered callbacks
+        // for sending analytics events
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_START) {
+                currentOnStart()
+            } else if (event == Lifecycle.Event.ON_STOP) {
+                currentOnStop()
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+    /* Home screen content */
+}
+// [END android_compose_side_effects_disposableeffect]
+
+// [START android_compose_side_effects_sideeffect]
+@Composable
+fun rememberFirebaseAnalytics(user: User): FirebaseAnalytics {
+    val analytics: FirebaseAnalytics = remember {
+        FirebaseAnalytics()
+    }
+
+    // On every successful composition, update FirebaseAnalytics with
+    // the userType from the current User, ensuring that future analytics
+    // events have this metadata attached
+    SideEffect {
+        analytics.setUserProperty("userType", user.userType)
+    }
+    return analytics
+}
+// [END android_compose_side_effects_sideeffect]
+
+// [START android_compose_side_effects_producestate]
+@Composable
+fun loadNetworkImage(
+    url: String,
+    imageRepository: ImageRepository
+): State<Result<Image>> {
+
+    // Creates a State<T> with Result.Loading as initial value
+    // If either `url` or `imageRepository` changes, the running producer
+    // will cancel and will be re-launched with the new inputs.
+    return produceState<Result<Image>>(initialValue = Result.Loading, url, imageRepository) {
+
+        // In a coroutine, can make suspend calls
+        val image = imageRepository.load(url)
+
+        // Update State with either an Error or Success result.
+        // This will trigger a recomposition where this State is read
+        value = if (image == null) {
+            Result.Error
+        } else {
+            Result.Success(image)
+        }
+    }
+}
+// [START_EXCLUDE silent]
+class ImageRepository {
+    fun load(url: String): Image? { return null }
+}
+sealed class Result<out T> {
+    object Loading : Result<Nothing>()
+    object Error : Result<Nothing>()
+    class Success<T>(t: T?) : Result<T>()
+}
+// [END_EXCLUDE]
+// [END android_compose_side_effects_producestate]
+
+// [START android_compose_side_effects_derivedstateof]
+@Composable
+fun TodoList(highPriorityKeywords: List<String> = listOf("Review", "Unblock", "Compose")) {
+
+    val todoTasks = remember { mutableStateListOf<String>() }
+
+    // Calculate high priority tasks only when the todoTasks or highPriorityKeywords
+    // change, not on every recomposition
+    val highPriorityTasks by remember(highPriorityKeywords) {
+        derivedStateOf {
+            todoTasks.filter { task ->
+                highPriorityKeywords.any { keyword ->
+                    task.contains(keyword)
+                }
+            }
+        }
+    }
+
+    Box(Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(highPriorityTasks) { /* ... */ }
+            items(todoTasks) { /* ... */ }
+        }
+        /* Rest of the UI where users can add elements to the list */
+    }
+}
+// [END android_compose_side_effects_derivedstateof]
