a3d85e64c312f7a24d47eba848d82088b6e137c0
riggaroo <riggaroo@users.noreply.github.com>
2023-02-23 14:51:35 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/layouts/ConstraintLayoutSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layouts/ConstraintLayoutSnippets.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 @file:Suppress("UNUSED_VARIABLE")
 
 package com.example.compose.snippets.layouts
@@ -49,13 +65,15 @@ fun ConstraintLayoutContent() {
 
         // Assign reference "text" to the Text composable
         // and constrain it to the bottom of the Button composable
-        Text("Text", Modifier.constrainAs(text) {
-            top.linkTo(button.bottom, margin = 16.dp)
-        })
+        Text(
+            "Text",
+            Modifier.constrainAs(text) {
+                top.linkTo(button.bottom, margin = 16.dp)
+            }
+        )
     }
 }
 // [END android_compose_constraintlayout_basics]
-
 
 // [START android_compose_constraintlayout_decoupled]
 @Composable
