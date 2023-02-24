82be0b8c8133f24e35e0e03b76ffb9b39c9db4b9
Chris Arriola <carriola@google.com>
2023-02-23 17:06:02 -0800

    Add snippets for /layouts/material

--- compose/snippets/src/main/java/com/example/compose/snippets/layout/LayoutBasicsSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layout/LayoutBasicsSnippets.kt
@@ -30,7 +30,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ModalNavigationDrawer
