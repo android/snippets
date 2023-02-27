a242b274b6ba627f1961478f47056afad6ac748e
Chris Arriola <carriola@google.com>
2023-02-27 15:36:26 -0800

    Update package to layouts/ (#78)
    * Update package to layouts/
    
    * Apply Spotless
    
    ---------
    
    Co-authored-by: arriolac <arriolac@users.noreply.github.com>

--- compose/snippets/src/main/java/com/example/compose/snippets/layouts/LayoutBasicsSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layouts/LayoutBasicsSnippets.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.compose.snippets.layout
+package com.example.compose.snippets.layouts
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
