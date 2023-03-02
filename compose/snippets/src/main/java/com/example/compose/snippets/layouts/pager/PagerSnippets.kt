5ca557c0c4aeb6ef66c22468088571ced76196e6
Rebecca Franks <riggaroo@google.com>
2023-03-02 14:24:37 +0000

    Add pager snippets

--- compose/snippets/src/main/java/com/example/compose/snippets/layouts/pager/PagerSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layouts/pager/PagerSnippets.kt
@@ -1,16 +1,11 @@
 package com.example.compose.snippets.layouts.pager
 
-import android.inputmethodservice.Keyboard
 import android.util.Log
-import androidx.annotation.DrawableRes
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,16 +27,9 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.viewpager.widget.PagerAdapter
-import com.example.compose.snippets.R
 import kotlinx.coroutines.launch
 
 /*
