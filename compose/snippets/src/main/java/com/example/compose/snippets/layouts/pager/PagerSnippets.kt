2c81ff427b9d40cd3fc61d06103c43f987dbe11a
Rebecca Franks <riggaroo@google.com>
2023-03-03 10:24:34 +0000

    Remove kotlin extensions and upgrade android compiler

--- compose/snippets/src/main/java/com/example/compose/snippets/layouts/pager/PagerSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layouts/pager/PagerSnippets.kt
@@ -23,9 +23,11 @@ package com.example.compose.snippets.layouts.pager
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -33,13 +35,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -52,10 +60,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.example.compose.snippets.util.rememberRandomSampleImageUrl
 import kotlin.math.absoluteValue
 import kotlinx.coroutines.launch
 
@@ -370,3 +382,56 @@ private val threePagesPerViewport = object : PageSize {
     }
 }
 // [END android_compose_pager_custom_page_size]
+
+@Preview
+@Composable
+private fun CustomSnapDistance() {
+    // [START android_compose_pager_custom_snap_distance]
+    val pagerState = rememberPagerState()
+
+    val fling = PagerDefaults.flingBehavior(
+        state = pagerState,
+        pagerSnapDistance = PagerSnapDistance.atMost(10)
+    )
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            state = pagerState,
+            pageCount = 10,
+            pageSize = PageSize.Fixed(200.dp),
+            beyondBoundsPageCount = 10,
+            flingBehavior = fling
+        ) {
+            PagerSampleItem(page = it)
+        }
+    }
+    // [END android_compose_pager_custom_snap_distance]
+}
+
+@Composable
+internal fun PagerSampleItem(
+    page: Int,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier.fillMaxSize()) {
+        // Our page content, displaying a random image
+        Image(
+            painter = rememberAsyncImagePainter(model = rememberRandomSampleImageUrl(width = 600)),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier.matchParentSize()
+        )
+
+        // Displays the page index
+        Text(
+            text = page.toString(),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.surface, RoundedCornerShape(4.dp))
+                .sizeIn(minWidth = 40.dp, minHeight = 40.dp)
+                .padding(8.dp)
+                .wrapContentSize(Alignment.Center)
+        )
+    }
+}
