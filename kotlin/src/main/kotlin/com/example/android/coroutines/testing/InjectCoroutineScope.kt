15afe7fed1f6527aa5f68dca932e89a0b29eafd3
Márton Braun <zsmb@google.com>
2023-03-03 18:32:13 +0100

    Simplify UserStateTest

--- kotlin/src/main/kotlin/com/example/android/coroutines/testing/InjectCoroutineScope.kt
+++ kotlin/src/main/kotlin/com/example/android/coroutines/testing/InjectCoroutineScope.kt
@@ -1,43 +1,43 @@
 package com.example.android.coroutines.testing.scope
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 interface UserRepository {
-    fun getAllUsers(): Flow<List<String>>
+    suspend fun getAllUsers(): List<String>
     suspend fun register(name: String)
 }
 
 class FakeUserRepository : UserRepository {
-    private var users = MutableStateFlow(listOf<String>())
+    private var users = listOf<String>()
 
-    override fun getAllUsers(): Flow<List<String>> = users
+    override suspend fun getAllUsers(): List<String> = users
 
     override suspend fun register(name: String) {
-        users.update { users -> users + name }
+        delay(100L) // Simulate work
+        users = users + name
         println("Registered $name")
     }
 }
 
+// [START android_coroutine_test_user_state]
 class UserState(
     private val userRepository: UserRepository,
     private val scope: CoroutineScope,
 ) {
-    val users = userRepository.getAllUsers()
-        .stateIn(
-            scope = scope,
-            started = SharingStarted.WhileSubscribed(5000L),
-            initialValue = emptyList()
-        )
+    private val _users = MutableStateFlow(emptyList<String>())
+    val users: StateFlow<List<String>> = _users.asStateFlow()
 
     fun registerUser(name: String) {
         scope.launch {
             userRepository.register(name)
+            _users.update { userRepository.getAllUsers() }
         }
     }
 }
+// [END android_coroutine_test_user_state]
--- kotlin/src/test/kotlin/com/example/android/coroutines/testing/UserStateTest.kt
+++ kotlin/src/test/kotlin/com/example/android/coroutines/testing/UserStateTest.kt
@@ -5,28 +5,23 @@ package com.example.android.coroutines.testing
 import com.example.android.coroutines.testing.scope.FakeUserRepository
 import com.example.android.coroutines.testing.scope.UserState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 import org.junit.Test
 
+// [START android_coroutine_test_user_state_test]
 class UserStateTest {
-    val testScope = TestScope()
-
     @Test
-    fun addUserTest() = testScope.runTest {
+    fun addUserTest() = runTest { // this: TestScope
         val repository = FakeUserRepository()
-        val userState = UserState(repository, backgroundScope)
-
-        // Add an empty collector so that stateIn is active
-        backgroundScope.launch { userState.users.collect() }
+        val userState = UserState(repository, scope = this)
 
         userState.registerUser("Mona")
-        runCurrent() // Let the coroutine complete and changes propagate
+        advanceUntilIdle() // Let the coroutine complete and changes propagate
 
         assertEquals(listOf("Mona"), userState.users.value)
     }
 }
+// [END android_coroutine_test_user_state_test]
