daac227caee8aef07729b15e1e7d339457121107
riggaroo <riggaroo@users.noreply.github.com>
2023-06-02 12:25:07 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/system/CutoutSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/system/CutoutSnippets.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.system
 
 import android.os.Bundle
@@ -10,15 +26,12 @@ import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import com.example.compose.snippets.R
 
 /*
 * Copyright 2022 The Android Open Source Project
@@ -54,7 +67,7 @@ class CutoutExampleSnippet : ComponentActivity() {
 @Composable
 fun CutoutManualHandling() {
     // [START android_compose_cutouts_manual]
-    Canvas(modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.displayCutout)){
+    Canvas(modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.displayCutout)) {
         drawRect(Color.Red, style = Stroke(2.dp.toPx()))
     }
     // [END android_compose_cutouts_manual]
