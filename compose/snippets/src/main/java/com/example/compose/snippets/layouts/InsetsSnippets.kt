3553c3907ec201fa530b47b7ab126eebd7868293
Rebecca Franks <riggaroo@google.com>
2023-06-02 09:45:44 +0100

    Add M3 insets Snippets

--- compose/snippets/src/main/java/com/example/compose/snippets/layouts/InsetsSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/layouts/InsetsSnippets.kt
@@ -21,6 +21,7 @@ package com.example.compose.snippets.layouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -29,6 +30,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.only
@@ -40,8 +43,13 @@ import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
@@ -113,4 +121,44 @@ fun ConsumedFromPaddingSnippet() {
         Spacer(Modifier.windowInsetsBottomHeight(WindowInsets.ime))
     }
     // [END android_compose_insets_consumed_from_padding]
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Preview
+@Composable
+fun M3SupportScaffoldSnippet() {
+    val colors = listOf(Color.Red, Color.Blue, Color.Yellow)
+    // [START android_compose_insets_m3_scaffold]
+    Scaffold { innerPadding ->
+        // innerPadding contains inset information for you to use and apply
+        LazyColumn(
+            // consume insets as scaffold doesn't do it by default
+            modifier = Modifier.consumeWindowInsets(innerPadding),
+            contentPadding = innerPadding
+        ) {
+            items(count = 100) {
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .background(colors[it % colors.size])
+                )
+            }
+        }
+    }
+    // [END android_compose_insets_m3_scaffold]
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun OverrideDefaultInsetsSnippet() {
+    // [START android_compose_insets_override_defaults]
+    LargeTopAppBar(
+        windowInsets = WindowInsets(0, 0, 0, 0),
+        title = {
+            Text("Hi")
+        })
+    // [END android_compose_insets_override_defaults]
 }
