ee94cc88dd358ce10c0b1b47ce40a64ede9c2d28
Rebecca Franks <riggaroo@google.com>
2023-08-10 14:56:39 +0100

    Added scale on shared axis

--- compose/snippets/src/main/java/com/example/compose/snippets/animations/AnimationCookbook.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/animations/AnimationCookbook.kt
@@ -16,23 +16,24 @@
 
 package com.example.compose.snippets.animations
 
-import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.EaseInOut
+import androidx.compose.animation.core.CubicBezierEasing
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -43,9 +44,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -58,14 +70,21 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.compose.AsyncImage
+import com.example.compose.snippets.util.randomSampleImageUrl
+import java.net.URLDecoder
+import java.net.URLEncoder
 import kotlin.math.roundToInt
 
 /*
@@ -218,9 +237,11 @@ fun AnimatePadding() {
 @Preview
 @Composable
 fun AnimateSizeChange() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         // [START android_compose_animation_size_change]
         var expanded by remember { mutableStateOf(false) }
         Box(
@@ -246,12 +267,14 @@ fun AnimateSizeChange() {
 @Preview
 @Composable
 fun AnimateOffset() {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
         // [START android_compose_animation_size_change]
         var moved by remember { mutableStateOf(false) }
-        val pxToMove = with(LocalDensity.current){
+        val pxToMove = with(LocalDensity.current) {
             100.dp.toPx().roundToInt()
         }
         val offset by animateIntOffsetAsState(
@@ -285,49 +308,33 @@ fun AnimateOffset() {
 fun AnimateBetweenComposableDestinations() {
     // [START android_compose_animate_destinations]
     val navController = rememberNavController()
+    val customBezierCurve = CubicBezierEasing(1.0f, 0f, 0.6f, 1f)
     NavHost(
         navController = navController, startDestination = "landing",
-        enterTransition = { EnterTransition.None },
-        exitTransition = { ExitTransition.Hold }
-    ) {
-        val animationSpec = tween<IntOffset>(500, easing = EaseInOut)
-        composable("landing") {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xff53D9A1))
-                    .pointerInput(Unit) {
-                        detectTapGestures {
-                            navController.navigate("detail")
-                        }
-                    }
-            ) {
-                Text("Landing", modifier = Modifier.padding(16.dp))
-            }
+        enterTransition = {
+            fadeIn(animationSpec = tween(60, delayMillis = 60, easing = LinearEasing
+            )) + scaleIn(animationSpec = tween(300, easing = customBezierCurve), initialScale = 0.8f)
+        },
+        exitTransition = {
+           scaleOut(animationSpec = tween(300, easing = customBezierCurve), targetScale = 1.1f)
+        },
+        popExitTransition = {
+            scaleOut(animationSpec = tween(300, easing = customBezierCurve), targetScale = 0.8f)
         }
-        composable("detail", enterTransition = {
-            slideIntoContainer(
-                AnimatedContentTransitionScope.SlideDirection.Start,
-                animationSpec = animationSpec
-            )
-        }, popExitTransition = {
-            slideOutOfContainer(
-                AnimatedContentTransitionScope.SlideDirection.End,
-                animationSpec = animationSpec
-            )
-        }) {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF4FC3F7))
-                    .pointerInput(Unit) {
-                        detectTapGestures {
-                            navController.popBackStack()
-                        }
-                    }
-            ) {
-                Text("Detail", modifier = Modifier.padding(16.dp))
-            }
+    ) {
+        composable("landing") {
+            ScreenLanding(onItemClicked = {
+                navController.navigate("detail/${URLEncoder.encode(it)}")
+            })
+        }
+        composable("detail/{photoUrl}",
+            arguments = listOf(navArgument("photoUrl") { type = NavType.StringType })
+        ) { backStackEntry ->
+            ScreenDetails(
+                photo = URLDecoder.decode(backStackEntry.arguments!!.getString("photoUrl")!!),
+                onBackClicked = {
+                    navController.popBackStack()
+                })
         }
     }
     // [END android_compose_animate_destinations]
@@ -338,8 +345,11 @@ fun AnimateBetweenComposableDestinations() {
 fun AnimateSizeChange_Specs() {
     Row(modifier = Modifier.fillMaxSize()) {
         var expanded by remember { mutableStateOf(false) }
-        Column(modifier = Modifier
-            .padding(8.dp).weight(1f)) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .weight(1f)
+        ) {
             Text("No spec set")
             Box(
                 modifier = Modifier
@@ -358,14 +368,22 @@ fun AnimateSizeChange_Specs() {
 
             }
         }
-        Column(modifier = Modifier
-            .padding(8.dp).weight(1f)) {
+        Column(
+            modifier = Modifier
+                .padding(8.dp)
+                .weight(1f)
+        ) {
             Text("Custom spec")
             // [START android_compose_animation_size_change_spec]
             Box(
                 modifier = Modifier
                     .background(colorBlue)
-                    .animateContentSize(spring(stiffness = Spring.StiffnessLow, dampingRatio = Spring.DampingRatioHighBouncy))
+                    .animateContentSize(
+                        spring(
+                            stiffness = Spring.StiffnessLow,
+                            dampingRatio = Spring.DampingRatioHighBouncy
+                        )
+                    )
                     .height(if (expanded) 300.dp else 200.dp)
                     .fillMaxSize()
                     .clickable(
@@ -386,3 +404,133 @@ fun AnimateSizeChange_Specs() {
 
 val colorGreen = Color(0xFF53D9A1)
 val colorBlue = Color(0xFF4FC3F7)
+
+@Preview
+@Composable
+fun AnimationLayoutIndividualItem() {
+    var toggled by remember {
+        mutableStateOf(false)
+    }
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .clickable(indication = null, interactionSource = interactionSource) {
+            toggled = !toggled
+        }) {
+        val offset = animateIntOffsetAsState(
+            targetValue = if (toggled) {
+                IntOffset(150, 150)
+            } else {
+                IntOffset.Zero
+            }, label = "offset"
+        )
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(colorBlue)
+        )
+        Box(modifier = Modifier
+            .layout { measurable, constraints ->
+                val placeable = measurable.measure(constraints)
+                layout(placeable.width + offset.value.x, placeable.height + offset.value.y) {
+                    placeable.placeRelative(offset.value)
+                }
+            }
+            .size(100.dp)
+            .background(colorGreen)
+        )
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .background(colorBlue)
+        )
+    }
+
+}
+
+@Composable
+private fun ScreenLanding(onItemClicked: (String) -> Unit) {
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Adaptive(200.dp),
+        verticalItemSpacing = 4.dp,
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        content = {
+            items(randomSizedPhotos) { photo ->
+                AsyncImage(
+                    model = photo,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .clickable {
+                            onItemClicked(photo)
+                        }
+                )
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ScreenDetails(photo: String, onBackClicked: () -> Unit) {
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = {
+                Text("Photo Details")
+            },
+                navigationIcon = {
+                    IconButton(onClick = { onBackClicked() }) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+                    }
+                }
+            )
+        }
+    ) { padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            AsyncImage(
+                model = photo,
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+
+            )
+            Text("Photo details")
+        }
+    }
+}
+
+private val randomSizedPhotos = listOf(
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 900, height = 1600),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 300, height = 400),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 900, height = 1600),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 300, height = 400),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 900, height = 1600),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 300, height = 400),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 300, height = 400),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 900, height = 1600),
+    randomSampleImageUrl(width = 500, height = 500),
+    randomSampleImageUrl(width = 300, height = 400),
+    randomSampleImageUrl(width = 1600, height = 900),
+    randomSampleImageUrl(width = 500, height = 500),
+)
