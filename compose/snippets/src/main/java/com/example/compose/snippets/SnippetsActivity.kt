c06ad2ecb6c7271feb9cdc070885ed72823f0e79
Rebecca Franks <riggaroo@google.com>
2024-01-04 14:31:53 +0000

    Adding graphics-shape snippets

--- compose/snippets/src/main/java/com/example/compose/snippets/SnippetsActivity.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/SnippetsActivity.kt
@@ -38,6 +38,8 @@ import com.example.compose.snippets.components.ProgressIndicatorExamples
 import com.example.compose.snippets.components.ScaffoldExample
 import com.example.compose.snippets.components.SliderExamples
 import com.example.compose.snippets.components.SwitchExamples
+import com.example.compose.snippets.graphics.ApplyPolygonAsClipBasic
+import com.example.compose.snippets.graphics.ApplyPolygonAsClipImage
 import com.example.compose.snippets.graphics.BitmapFromComposableSnippet
 import com.example.compose.snippets.graphics.BrushExamplesScreen
 import com.example.compose.snippets.images.ImageExamplesScreen
@@ -75,6 +77,7 @@ class SnippetsActivity : ComponentActivity() {
                                             it.route
                                         )
                                     }
+                                    Destination.ShapesExamples -> ApplyPolygonAsClipImage()
                                 }
                             }
                         }
--- compose/snippets/src/main/java/com/example/compose/snippets/graphics/ShapesSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/graphics/ShapesSnippets.kt
@@ -1,5 +1,6 @@
 package com.example.compose.snippets.graphics
 
+import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
@@ -8,6 +9,7 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -16,18 +18,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
@@ -36,6 +44,9 @@ import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.Cubic
 import androidx.graphics.shapes.Morph
 import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.star
+import com.example.compose.snippets.R
+import kotlin.math.min
 
 
 @Preview
@@ -158,7 +169,8 @@ private fun MorphExampleAnimation() {
         targetValue = 1f,
         animationSpec = infiniteRepeatable(
             tween(500),
-            repeatMode = RepeatMode.Reverse),
+            repeatMode = RepeatMode.Reverse
+        ),
         label = "morph"
     )
     Box(modifier = Modifier
@@ -192,6 +204,7 @@ private fun MorphExampleAnimation() {
         .fillMaxSize())
     // [END android_compose_graphics_polygon_morph_animation]
 }
+
 /**
  * Transforms the morph at a given progress into a [Path].
  * It can optionally be scaled, using the origin (0,0) as pivot point.
@@ -232,39 +245,44 @@ fun List<Cubic>.toPath(path: Path = Path()): Path {
     path.close()
     return path
 }
-
+// [START android_compose_morph_clip_shape]
 class MorphPolygonShape(
     private val morph: Morph,
-    private val percentage: Float) : Shape {
+    private val percentage: Float
+) : Shape {
 
+    private val matrix = Matrix()
     override fun createOutline(
         size: Size,
         layoutDirection: LayoutDirection,
         density: Density
     ): Outline {
-        /*val matrixA = calculateMatrix(shapeA.bounds, size.width, size.height)
-        shapeA.transform(matrixA)
-        val matrixB = calculateMatrix(shapeB.bounds, size.width, size.height)
-        shapeB.transform(matrixB)
-        val morph = Morph(shapeA, shapeB)
-        morph.progress = percentage*/
-        // todo figure out how to scale and offset the shapes here
-        val path = morph.toComposePath(progress = percentage, scale = size.width)
+        // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+        // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+        matrix.scale(size.width / 2f, size.height / 2f)
+        matrix.translate(1f, 1f)
 
+        val path = morph.toComposePath(progress = percentage)
+        path.transform(matrix)
         return Outline.Generic(path)
     }
 }
+// [END android_compose_morph_clip_shape]
 @Preview
 @Composable
 private fun MorphOnClick() {
     // [START android_compose_graphics_morph_on_click]
     val shapeA = remember {
-        RoundedPolygon(5,
-            rounding = CornerRounding(0.2f))
+        RoundedPolygon(
+            6,
+            rounding = CornerRounding(0.2f)
+        )
     }
     val shapeB = remember {
-        RoundedPolygon(3,
-            rounding = CornerRounding(0.3f))
+        RoundedPolygon.star(
+            6,
+            rounding = CornerRounding(0.1f)
+        )
     }
     val morph = remember {
         Morph(shapeA, shapeB)
@@ -273,11 +291,13 @@ private fun MorphOnClick() {
         MutableInteractionSource()
     }
     val isPressed by interactionSource.collectIsPressedAsState()
-    val animatedProgress = animateFloatAsState(targetValue = if (isPressed) 1f else 0f,
-        label = "progress", animationSpec = spring(dampingRatio = 0.4f, stiffness = Spring.StiffnessMedium)
+    val animatedProgress = animateFloatAsState(
+        targetValue = if (isPressed) 1f else 0f,
+        label = "progress",
+        animationSpec = spring(dampingRatio = 0.4f, stiffness = Spring.StiffnessMedium)
     )
     Box(modifier = Modifier
-        .fillMaxSize()
+        .size(200.dp)
         .padding(8.dp)
         .clip(MorphPolygonShape(morph, animatedProgress.value))
         .background(Color(0xFF80DEEA))
@@ -285,13 +305,162 @@ private fun MorphOnClick() {
         .clickable(interactionSource = interactionSource, indication = null) {
         }
     ) {
-        Text("Hello")
+        Text("Hello", modifier = Modifier.align(Alignment.Center))
     }
     // [END android_compose_graphics_morph_on_click]
 }
 
+
+// [START android_compose_shapes_polygon_compose_shape]
+class RoundedPolygonShape(
+    private val polygon: RoundedPolygon
+) : Shape {
+    private val matrix = Matrix()
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = polygon.cubics.toPath()
+        // below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+        // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+        matrix.scale(size.width / 2f, size.height / 2f)
+        matrix.translate(1f, 1f)
+        path.transform(matrix)
+
+        return Outline.Generic(path)
+    }
+}
+// [END android_compose_shapes_polygon_compose_shape]
+
 @Preview
 @Composable
-private fun ApplyPolygonAsClip() {
-
+fun ApplyPolygonAsClipBasic() {
+    // [START android_compose_shapes_apply_as_clip]
+    val hexagon = remember {
+        RoundedPolygon(
+            6,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val clip = remember(hexagon) {
+        RoundedPolygonShape(polygon = hexagon)
+    }
+    Box(
+        modifier = Modifier
+            .clip(clip)
+            .background(MaterialTheme.colorScheme.secondary)
+            .size(200.dp)
+    ) {
+        Text(
+            "Hello Compose",
+            color = MaterialTheme.colorScheme.onSecondary,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+    // [END android_compose_shapes_apply_as_clip]
 }
+
+@Preview
+@Composable
+fun ApplyPolygonAsClipImage() {
+    // [START android_compose_shapes_apply_as_clip_advanced]
+    val hexagon = remember {
+        RoundedPolygon(
+            6,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val clip = remember(hexagon) {
+        RoundedPolygonShape(polygon = hexagon)
+    }
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(id = R.drawable.dog),
+            contentDescription = "Dog",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .graphicsLayer {
+                    this.shadowElevation = 6.dp.toPx()
+                    this.shape = clip
+                    this.clip = true
+                    this.ambientShadowColor = Color.Black
+                    this.spotShadowColor = Color.Black
+                }
+                .size(200.dp)
+
+        )
+    }
+    // [END android_compose_shapes_apply_as_clip_advanced]
+}
+
+// [START android_compose_shapes_custom_rotating_morph_shape]
+class CustomRotatingMorphShape(
+    private val morph: Morph,
+    private val percentage: Float,
+    private val rotation: Float
+) : Shape {
+
+    private val matrix = Matrix()
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        // Below assumes that you haven't changed the default radius of 1f, nor the centerX and centerY of 0f
+        // By default this stretches the path to the size of the container, if you don't want stretching, use the same size.width for both x and y.
+        matrix.scale(size.width / 2f, size.height / 2f)
+        matrix.translate(1f, 1f)
+        matrix.rotateZ(rotation)
+
+        val path = morph.toComposePath(progress = percentage)
+        path.transform(matrix)
+
+        return Outline.Generic(path)
+    }
+}
+@Preview
+@Composable
+private fun RotatingScallopedProfilePic() {
+    val shapeA = remember {
+        RoundedPolygon(
+            12,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val shapeB = remember {
+        RoundedPolygon.star(
+            12,
+            rounding = CornerRounding(0.2f)
+        )
+    }
+    val morph = remember {
+        Morph(shapeA, shapeB)
+    }
+    val infiniteTransition = rememberInfiniteTransition("infinite outline movement")
+    val animatedProgress = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(tween(2000, easing = LinearEasing), repeatMode = RepeatMode.Reverse),
+        label = "animatedMorphProgress"
+    )
+    val animatedRotation = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(tween(6000, easing = LinearEasing), repeatMode = RepeatMode.Reverse),
+        label = "animatedMorphProgress"
+    )
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Image(
+            painter = painterResource(id = R.drawable.dog),
+            contentDescription = "Dog",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(CustomRotatingMorphShape(morph, animatedProgress.value, animatedRotation.value))
+                .size(200.dp)
+        )
+    }
+}
+// [END android_compose_shapes_custom_rotating_morph_shape]
