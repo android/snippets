5daa609a4b527adfcd09922202c6372877fa2260
Rebecca Franks <riggaroo@google.com>
2024-01-04 17:54:16 +0000

    Added custom polygon

--- compose/snippets/src/main/java/com/example/compose/snippets/graphics/ShapesSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/graphics/ShapesSnippets.kt
@@ -16,6 +16,9 @@
 
 package com.example.compose.snippets.graphics
 
+import android.graphics.PointF
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
@@ -43,12 +46,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Matrix
 import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -56,13 +65,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.flatten
+import androidx.core.graphics.plus
+import androidx.core.graphics.times
 import androidx.graphics.shapes.CornerRounding
 import androidx.graphics.shapes.Cubic
 import androidx.graphics.shapes.Morph
 import androidx.graphics.shapes.RoundedPolygon
 import androidx.graphics.shapes.star
 import com.example.compose.snippets.R
-import kotlin.math.min
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Preview
 @Composable
@@ -255,21 +269,22 @@ fun Morph.toComposePath(progress: Float, scale: Float = 1f, path: Path = Path())
 /**
  * Function used to create a Path from a list of Cubics.
  */
-fun List<Cubic>.toPath(path: Path = Path()): Path {
+fun List<Cubic>.toPath(path: Path = Path(), scale: Float = 1f): Path {
     path.rewind()
     firstOrNull()?.let { first ->
-        path.moveTo(first.anchor0X, first.anchor0Y)
+        path.moveTo(first.anchor0X * scale, first.anchor0Y * scale)
     }
     for (bezier in this) {
         path.cubicTo(
-            bezier.control0X, bezier.control0Y,
-            bezier.control1X, bezier.control1Y,
-            bezier.anchor1X, bezier.anchor1Y
+            bezier.control0X * scale, bezier.control0Y * scale,
+            bezier.control1X * scale, bezier.control1Y * scale,
+            bezier.anchor1X * scale, bezier.anchor1Y * scale
         )
     }
     path.close()
     return path
 }
+
 // [START android_compose_morph_clip_shape]
 class MorphPolygonShape(
     private val morph: Morph,
@@ -292,6 +307,7 @@ class MorphPolygonShape(
         return Outline.Generic(path)
     }
 }
+
 // [END android_compose_morph_clip_shape]
 @Preview
 @Composable
@@ -399,8 +415,10 @@ fun ApplyPolygonAsClipImage() {
     val clip = remember(hexagon) {
         RoundedPolygonShape(polygon = hexagon)
     }
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.dog),
             contentDescription = "Dog",
@@ -445,6 +463,7 @@ class CustomRotatingMorphShape(
         return Outline.Generic(path)
     }
 }
+
 @Preview
 @Composable
 private fun RotatingScallopedProfilePic() {
@@ -467,25 +486,176 @@ private fun RotatingScallopedProfilePic() {
     val animatedProgress = infiniteTransition.animateFloat(
         initialValue = 0f,
         targetValue = 1f,
-        animationSpec = infiniteRepeatable(tween(2000, easing = LinearEasing), repeatMode = RepeatMode.Reverse),
+        animationSpec = infiniteRepeatable(
+            tween(2000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
         label = "animatedMorphProgress"
     )
     val animatedRotation = infiniteTransition.animateFloat(
         initialValue = 0f,
         targetValue = 360f,
-        animationSpec = infiniteRepeatable(tween(6000, easing = LinearEasing), repeatMode = RepeatMode.Reverse),
+        animationSpec = infiniteRepeatable(
+            tween(6000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        ),
         label = "animatedMorphProgress"
     )
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
         Image(
             painter = painterResource(id = R.drawable.dog),
             contentDescription = "Dog",
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .clip(CustomRotatingMorphShape(morph, animatedProgress.value, animatedRotation.value))
+                .clip(
+                    CustomRotatingMorphShape(
+                        morph,
+                        animatedProgress.value,
+                        animatedRotation.value
+                    )
+                )
                 .size(200.dp)
         )
     }
 }
 // [END android_compose_shapes_custom_rotating_morph_shape]
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun ShapeAsLoader() {
+    // [START android_compose_graphics_basic_polygon]
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
+    val progress = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(tween(400), repeatMode = RepeatMode.Reverse),
+        label = "progress"
+    )
+    val pathMeasurer = remember {
+        PathMeasure()
+    }
+    Box(
+        modifier = Modifier
+            .padding(16.dp)
+            .drawWithCache {
+                val roundedPolygon = RoundedPolygon(
+                    numVertices = 6,
+                    radius = size.width / 2,
+                    centerX = size.width / 2,
+                    centerY = size.height / 2,
+                )
+                val roundedPolygonPath = roundedPolygon.cubics
+                    .toPath()
+                val androidPath = roundedPolygonPath
+                    .asAndroidPath()
+                val flattenedPath = androidPath.flatten()
+                pathMeasurer.setPath(roundedPolygonPath, false)
+                val totalLength = pathMeasurer.length
+
+                onDrawBehind {
+                    val currentLength = totalLength * progress.value
+                    flattenedPath.forEach { line ->
+                        if (line.startFraction * totalLength < currentLength) {
+                            drawLine(
+                                color = Color.Black,
+                                start = Offset(line.start.x, line.start.y),
+                                end = Offset(line.end.x, line.end.y),
+                                strokeWidth = 30f,
+                                cap = StrokeCap.Round
+                            )
+                        }
+                    }
+                    /*drawPath(
+                        roundedPolygonPath,
+                        color = Color.Blue,
+                        style = Stroke(width = 2.dp.toPx())
+                    )*/
+                }
+            }
+            .fillMaxSize()
+    )
+    // [END android_compose_graphics_basic_polygon]
+}
+
+@Preview
+@Composable
+private fun CartesianPoints() {
+    // [START android_compose_shapes_custom_vertices]
+    val vertices = remember {
+        val radius = 1f
+        val radiusSides = 0.8f
+        val innerRadius = .1f
+        floatArrayOf(
+            radialToCartesian(radiusSides, 0f.toRadians()).x,
+            radialToCartesian(radiusSides, 0f.toRadians()).y,
+            radialToCartesian(radius, 90f.toRadians()).x,
+            radialToCartesian(radius, 90f.toRadians()).y,
+            radialToCartesian(radiusSides, 180f.toRadians()).x,
+            radialToCartesian(radiusSides, 180f.toRadians()).y,
+            radialToCartesian(radius, 250f.toRadians()).x,
+            radialToCartesian(radius, 250f.toRadians()).y,
+            radialToCartesian(innerRadius, 270f.toRadians()).x,
+            radialToCartesian(innerRadius, 270f.toRadians()).y,
+            radialToCartesian(radius, 290f.toRadians()).x,
+            radialToCartesian(radius, 290f.toRadians()).y,
+        )
+    }
+    // [END android_compose_shapes_custom_vertices]
+
+    // [START android_compose_shapes_custom_vertices_draw]
+    val rounding = remember {
+        val roundingNormal = 0.6f
+        val roundingNone = 0f
+        listOf(
+            CornerRounding(roundingNormal),
+            CornerRounding(roundingNone),
+            CornerRounding(roundingNormal),
+            CornerRounding(roundingNormal),
+            CornerRounding(roundingNone),
+            CornerRounding(roundingNormal),
+        )
+    }
+
+    val polygon = remember(vertices, rounding) {
+        RoundedPolygon(
+            vertices = vertices,
+            perVertexRounding = rounding
+        )
+    }
+    Box(
+        modifier = Modifier
+            .drawWithCache {
+                val roundedPolygonPath = polygon.cubics
+                    .toPath()
+                onDrawBehind {
+                    scale(size.width * 0.5f, size.width * 0.5f) {
+                        translate(size.width * 0.5f, size.height * 0.5f) {
+                            drawPath(roundedPolygonPath, color = Color(0xFFF15087))
+                        }
+                    }
+
+                }
+            }
+            .size(400.dp)
+    )
+    // [END android_compose_shapes_custom_vertices_draw]
+}
+
+// [START android_compose_shapes_radial_to_cartesian]
+internal fun Float.toRadians() = this * PI.toFloat() / 180f
+
+internal val PointZero = PointF(0f, 0f)
+internal fun radialToCartesian(
+    radius: Float,
+    angleRadians: Float,
+    center: PointF = PointZero
+) = directionVectorPointF(angleRadians) * radius + center
+
+
+internal fun directionVectorPointF(angleRadians: Float) =
+    PointF(cos(angleRadians), sin(angleRadians))
+// [END android_compose_shapes_radial_to_cartesian]
