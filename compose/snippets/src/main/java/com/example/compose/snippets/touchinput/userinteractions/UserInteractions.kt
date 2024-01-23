28afe0bd1f011600d3d6506a0fb196647bd5edbf
arriolac <arriolac@users.noreply.github.com>
2024-01-23 17:33:17 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/touchinput/userinteractions/UserInteractions.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/touchinput/userinteractions/UserInteractions.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.touchinput.userinteractions
 
 import androidx.compose.animation.core.Animatable
@@ -28,86 +44,86 @@ import kotlinx.coroutines.flow.collectLatest
 // [START android_compose_userinteractions_scale_indication]
 // [START android_compose_userinteractions_scale_indication_object]
 object ScaleIndication : Indication {
-  @Composable
-  override fun rememberUpdatedInstance(interactionSource: InteractionSource): IndicationInstance {
-    // key the remember against interactionSource, so if it changes we create a new instance
-    val instance = remember(interactionSource) { ScaleIndicationInstance() }
+    @Composable
+    override fun rememberUpdatedInstance(interactionSource: InteractionSource): IndicationInstance {
+        // key the remember against interactionSource, so if it changes we create a new instance
+        val instance = remember(interactionSource) { ScaleIndicationInstance() }
 
-    LaunchedEffect(interactionSource) {
-      interactionSource.interactions.collectLatest { interaction ->
-        when (interaction) {
-          is PressInteraction.Press -> instance.animateToPressed(interaction.pressPosition)
-          is PressInteraction.Release -> instance.animateToResting()
-          is PressInteraction.Cancel -> instance.animateToResting()
+        LaunchedEffect(interactionSource) {
+            interactionSource.interactions.collectLatest { interaction ->
+                when (interaction) {
+                    is PressInteraction.Press -> instance.animateToPressed(interaction.pressPosition)
+                    is PressInteraction.Release -> instance.animateToResting()
+                    is PressInteraction.Cancel -> instance.animateToResting()
+                }
+            }
         }
-      }
-    }
 
-    return instance
-  }
+        return instance
+    }
 }
 // [END android_compose_userinteractions_scale_indication_object]
 
 // [START android_compose_userinteractions_scale_indication_instance]
 private class ScaleIndicationInstance : IndicationInstance {
-  var currentPressPosition: Offset = Offset.Zero
-  val animatedScalePercent = Animatable(1f)
+    var currentPressPosition: Offset = Offset.Zero
+    val animatedScalePercent = Animatable(1f)
 
-  suspend fun animateToPressed(pressPosition: Offset) {
-    currentPressPosition = pressPosition
-    animatedScalePercent.animateTo(0.9f, spring())
-  }
-
-  suspend fun animateToResting() {
-    animatedScalePercent.animateTo(1f, spring())
-  }
-
-  override fun ContentDrawScope.drawIndication() {
-    scale(
-      scale = animatedScalePercent.value,
-      pivot = currentPressPosition
-    ) {
-      this@drawIndication.drawContent()
+    suspend fun animateToPressed(pressPosition: Offset) {
+        currentPressPosition = pressPosition
+        animatedScalePercent.animateTo(0.9f, spring())
     }
-  }
+
+    suspend fun animateToResting() {
+        animatedScalePercent.animateTo(1f, spring())
+    }
+
+    override fun ContentDrawScope.drawIndication() {
+        scale(
+            scale = animatedScalePercent.value,
+            pivot = currentPressPosition
+        ) {
+            this@drawIndication.drawContent()
+        }
+    }
 }
 // [END android_compose_userinteractions_scale_indication_instance]
 // [END android_compose_userinteractions_scale_indication]
 
 @Composable
 private fun RememberRippleExample() {
-  // [START android_compose_userinteractions_material_remember_ripple]
-  Box(
-    Modifier.clickable(
-      onClick = {},
-      interactionSource = remember { MutableInteractionSource() },
-      indication = rememberRipple()
-    )
-  ) {
-    // ...
-  }
-  // [END android_compose_userinteractions_material_remember_ripple]
+    // [START android_compose_userinteractions_material_remember_ripple]
+    Box(
+        Modifier.clickable(
+            onClick = {},
+            interactionSource = remember { MutableInteractionSource() },
+            indication = rememberRipple()
+        )
+    ) {
+        // ...
+    }
+    // [END android_compose_userinteractions_material_remember_ripple]
 }
 
 // [START android_compose_userinteractions_disabled_ripple_theme]
 private object DisabledRippleTheme : RippleTheme {
 
-  @Composable
-  override fun defaultColor(): Color = Color.Transparent
+    @Composable
+    override fun defaultColor(): Color = Color.Transparent
 
-  @Composable
-  override fun rippleAlpha(): RippleAlpha = RippleAlpha(0f, 0f, 0f, 0f)
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = RippleAlpha(0f, 0f, 0f, 0f)
 }
 
 // [START_EXCLUDE]
 @Composable
 private fun MyComposable() {
 // [END_EXCLUDE]
-  CompositionLocalProvider(LocalRippleTheme provides DisabledRippleTheme) {
-    Button {
-      // ...
+    CompositionLocalProvider(LocalRippleTheme provides DisabledRippleTheme) {
+        Button {
+            // ...
+        }
     }
-  }
 // [START_EXCLUDE silent]
 }
 // [END_EXCLUDE]
@@ -118,23 +134,22 @@ private val MyRippleAlpha = RippleAlpha(0.5f, 0.5f, 0.5f, 0.5f)
 // [START android_compose_userinteractions_disabled_ripple_theme_color_alpha]
 private object DisabledRippleThemeColorAndAlpha : RippleTheme {
 
+    @Composable
+    override fun defaultColor(): Color = Color.Red
 
-  @Composable
-  override fun defaultColor(): Color = Color.Red
-
-  @Composable
-  override fun rippleAlpha(): RippleAlpha = MyRippleAlpha
+    @Composable
+    override fun rippleAlpha(): RippleAlpha = MyRippleAlpha
 }
 
 // [START_EXCLUDE]
 @Composable
 private fun MyComposable2() {
 // [END_EXCLUDE]
-  CompositionLocalProvider(LocalRippleTheme provides DisabledRippleThemeColorAndAlpha) {
-    Button {
-      // ...
+    CompositionLocalProvider(LocalRippleTheme provides DisabledRippleThemeColorAndAlpha) {
+        Button {
+            // ...
+        }
     }
-  }
 // [START_EXCLUDE silent]
 }
 // [END_EXCLUDE]
