f5569ff4e54335dde915639cb24b680da8e82da9
Ben Trengrove <bentrengrove@google.com>
2024-02-05 14:48:26 +1100

    Add text snippets

--- compose/snippets/src/main/java/com/example/compose/snippets/text/TextSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/text/TextSnippets.kt
@@ -19,14 +19,28 @@
 package com.example.compose.snippets.text
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -36,6 +50,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -55,10 +70,13 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.Hyphens
+import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
@@ -577,7 +595,187 @@ private object TextEffectiveStateManagement2 {
     // [END android_compose_text_state_management]
 }
 
-private val firaSansFamily = FontFamily()
+private const val SAMPLE_LONG_TEXT =
+    "Jetpack Compose is Android’s modern toolkit for building native UI. " +
+        "It simplifies and accelerates UI development on Android bringing your apps " +
+        "to life with less code, powerful tools, and intuitive Kotlin APIs. " +
+        "It makes building Android UI faster and easier."
+@Composable
+@Preview
+fun LineBreakSample() {
+    MaterialTheme {
+        Box(
+            Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .width(600.dp)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Text(
+                    text = SAMPLE_LONG_TEXT,
+                    modifier = Modifier.width(130.dp),
+                    fontSize = 14.sp,
+                    style = TextStyle.Default.copy(
+                        lineBreak = LineBreak.Simple
+                    )
+                )
+                Text(
+                    text = SAMPLE_LONG_TEXT,
+                    modifier = Modifier.width(130.dp),
+                    fontSize = 14.sp,
+                    style = TextStyle.Default.copy(
+                        lineBreak = LineBreak.Paragraph
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SmallScreenTextSnippet() {
+    MaterialTheme {
+        Box(
+            Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxWidth()
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(10.dp)) {
+                Row(Modifier.fillMaxWidth()) {
+                    val smallScreenAdaptedParagraph =
+                        LineBreak.Paragraph.copy(strategy = LineBreak.Strategy.Balanced)
+                    Text(
+                        text = SAMPLE_LONG_TEXT,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .border(BorderStroke(1.dp, Color.Gray)),
+                        fontSize = 14.sp,
+                        style = TextStyle.Default.copy(
+                            lineBreak = smallScreenAdaptedParagraph
+                        )
+                    )
+                    Text(
+                        text = "Balanced",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.fillMaxWidth().align(Alignment.CenterVertically)
+                    )
+                }
+                Row(Modifier.fillMaxWidth()) {
+                    Text(
+                        text = SAMPLE_LONG_TEXT,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .border(BorderStroke(1.dp, Color.Gray)),
+                        fontSize = 14.sp,
+                        style = TextStyle.Default
+                    )
+                    Text(
+                        text = "Default",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.fillMaxWidth().align(Alignment.CenterVertically)
+                    )
+                }
+            }
+        }
+    }
+}
+
+private object CJKTextSnippet {
+    @Composable
+    fun CJKSample() {
+        val customTitleLineBreak = LineBreak(
+            strategy = LineBreak.Strategy.HighQuality,
+            strictness = LineBreak.Strictness.Strict,
+            wordBreak = LineBreak.WordBreak.Phrase
+        )
+        Text(
+            text = "あなたに寄り添う最先端のテクノロジー。",
+            modifier = Modifier.width(250.dp),
+            fontSize = 14.sp,
+            style = TextStyle.Default.copy(
+                lineBreak = customTitleLineBreak
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HyphenateTextSnippet() {
+    MaterialTheme {
+        Box(
+            Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .fillMaxWidth()
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier.padding(10.dp)) {
+                Row(Modifier.fillMaxWidth()) {
+                    Text(
+                        text = SAMPLE_LONG_TEXT,
+                        modifier = Modifier
+                            .width(130.dp)
+                            .border(BorderStroke(1.dp, Color.Gray)),
+                        fontSize = 14.sp,
+                        style = TextStyle.Default.copy(
+                            lineBreak = LineBreak.Paragraph,
+                            hyphens = Hyphens.None
+                        )
+                    )
+                    Text(
+                        text = "Hyphens - None",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.fillMaxWidth().align(Alignment.CenterVertically)
+                    )
+                }
+                Row(Modifier.fillMaxWidth()) {
+                    Text(
+                        text = SAMPLE_LONG_TEXT,
+                        modifier = Modifier
+                            .width(130.dp)
+                            .border(BorderStroke(1.dp, Color.Gray)),
+                        fontSize = 14.sp,
+                        style = TextStyle.Default.copy(
+                            lineBreak = LineBreak.Paragraph,
+                            hyphens = Hyphens.Auto
+                        )
+                    )
+                    Text(
+                        text = "Hyphens - Auto",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.fillMaxWidth().align(Alignment.CenterVertically)
+                    )
+                }
+            }
+        }
+    }
+}
+
+//object MarqueeSample {
+    @OptIn(ExperimentalFoundationApi::class)
+    @Preview(showBackground = true)
+    @Composable
+    fun BasicMarqueeSample() {
+        // Marquee only animates when the content doesn't fit in the max width.
+        Column(Modifier.width(400.dp)) {
+            Text("Learn about why it's great to use Jetpack Compose",
+                modifier = Modifier.basicMarquee(),
+                fontSize = 50.sp
+            )
+        }
+    }
+//}
+
+// Using null just sets the font family to default, which is easier than supplying
+// the actual font file in the snippets repo
+private val firaSansFamily = null
 
 val LightBlue = Color(0xFF0066FF)
 val Purple = Color(0xFF800080)
