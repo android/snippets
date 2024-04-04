03cb5dd64af7670a9ea9f507209eebde72370482
Rebecca Franks <riggaroo@google.com>
2024-04-04 09:20:45 +0100

    Switch to snapshot version.

--- compose/snippets/src/main/java/com/example/compose/snippets/animations/SharedElementSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/animations/SharedElementSnippets.kt
@@ -3,7 +3,10 @@
 package com.example.compose.snippets.animations
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.BoundsTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionLayout
 import androidx.compose.animation.core.Spring
@@ -14,17 +17,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeightIn
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -305,8 +320,10 @@ fun SharedElement_SharedBounds() {
         ) { targetState ->
             if (!targetState) {
                 Row(modifier = Modifier
-                    .sharedBounds(rememberSharedContentState(key = "bounds"),
-                        animatedVisibilityScope = this@AnimatedContent)
+                    .sharedBounds(
+                        rememberSharedContentState(key = "bounds"),
+                        animatedVisibilityScope = this@AnimatedContent
+                    )
                     .background(Color.Green.copy(alpha = 0.5f))
                     .padding(8.dp)
                     .clickable(remember { MutableInteractionSource() }, indication = null) {
@@ -334,8 +351,10 @@ fun SharedElement_SharedBounds() {
                 }
             } else {
                 Column(modifier = Modifier
-                    .sharedBounds(rememberSharedContentState(key = "bounds"),
-                        animatedVisibilityScope = this@AnimatedContent)
+                    .sharedBounds(
+                        rememberSharedContentState(key = "bounds"),
+                        animatedVisibilityScope = this@AnimatedContent
+                    )
                     .background(Color.Green.copy(alpha = 0.7f))
                     .padding(top = 200.dp, start = 16.dp, end = 16.dp)
                     .clickable(remember { MutableInteractionSource() }, indication = null) {
@@ -373,4 +392,226 @@ fun SharedElement_SharedBounds() {
         }
     }
     // [END android_compose_animations_shared_element_shared_bounds]
+}
+
+
+@Preview
+@Composable
+private fun SharedElement_Clipping() {
+    // [START android_compose_animations_shared_element_clipping]
+    var showDetails by remember {
+        mutableStateOf(false)
+    }
+    SharedTransitionLayout {
+        AnimatedContent(
+            showDetails,
+            label = "basic_transition"
+        ) { targetState ->
+            if (!targetState) {
+                Row(modifier = Modifier
+                    .sharedBounds(
+                        rememberSharedContentState(key = "bounds"),
+                        animatedVisibilityScope = this@AnimatedContent
+                    )
+                    .background(Color.Green.copy(alpha = 0.5f))
+                    .padding(8.dp)
+                    .clickable(remember { MutableInteractionSource() }, indication = null) {
+                        showDetails = true
+                    }
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.lizard),
+                        contentDescription = "Lizard",
+                        modifier = Modifier
+                            .size(100.dp)
+                            .sharedElement(
+                                rememberSharedContentState(key = "image"),
+                                animatedVisibilityScope = this@AnimatedContent,
+                                clipInOverlayDuringTransition = OverlayClip(RoundedCornerShape(16.dp))
+                            )
+                        /*.clip(RoundedCornerShape(16.dp))*/,
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(
+                        "Lorem ipsum dolor sit amet.", fontSize = 21.sp,
+                        modifier = Modifier.sharedElement(
+                            rememberSharedContentState(key = "title"),
+                            animatedVisibilityScope = this@AnimatedContent
+                        )
+                    )
+                }
+            } else {
+                Column(modifier = Modifier
+                    .sharedBounds(
+                        rememberSharedContentState(key = "bounds"),
+                        animatedVisibilityScope = this@AnimatedContent
+                    )
+                    .background(Color.Green.copy(alpha = 0.7f))
+                    .padding(top = 200.dp, start = 16.dp, end = 16.dp)
+                    .clickable(remember { MutableInteractionSource() }, indication = null) {
+                        showDetails = false
+                    }
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.lizard),
+                        contentDescription = "Lizard",
+                        modifier = Modifier
+                            .size(200.dp)
+                            .sharedElement(
+                                rememberSharedContentState(key = "image"),
+                                animatedVisibilityScope = this@AnimatedContent,
+                                clipInOverlayDuringTransition = OverlayClip(RoundedCornerShape(16.dp))
+                            )
+                        /*  .clip(RoundedCornerShape(16.dp))*/,
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(
+                        "Lorem ipsum dolor sit amet.", fontSize = 21.sp,
+                        modifier = Modifier.sharedElement(
+                            rememberSharedContentState(key = "title"),
+                            animatedVisibilityScope = this@AnimatedContent
+                        )
+                    )
+                    Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet lobortis velit. " +
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                                " Curabitur sagittis, lectus posuere imperdiet facilisis, nibh massa " +
+                                "molestie est, quis dapibus orci ligula non magna. Pellentesque rhoncus " +
+                                "hendrerit massa quis ultricies. Curabitur congue ullamcorper leo, at maximus"
+                    )
+                }
+            }
+        }
+    }
+    // [END android_compose_animations_shared_element_clipping]
+}
+
+
+@Preview
+@Composable
+private fun SharedElement_SkipLookaheadSize() {
+    // Nested shared bounds sample.
+    val selectionColor = Color(0xff3367ba)
+    var expanded by remember { mutableStateOf(true) }
+    SharedTransitionLayout(
+        Modifier
+            .fillMaxSize()
+            .clickable(interactionSource = remember { MutableInteractionSource() }, indication = null) {
+                expanded = !expanded
+            }
+            .background(Color(0x88000000))
+    ) {
+        AnimatedVisibility(
+            visible = expanded,
+            enter = EnterTransition.None,
+            exit = ExitTransition.None
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Surface(
+                    Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(20.dp)
+                        .sharedBounds(
+                            rememberSharedContentState(key = "container"),
+                            this@AnimatedVisibility
+                        )
+                        .requiredHeightIn(max = 60.dp),
+                    shape = RoundedCornerShape(50),
+                ) {
+                    Row(
+                        Modifier
+                            .padding(10.dp)
+                            // By using Modifier.skipToLookaheadSize(), we are telling the layout
+                            // system to layout the children of this node as if the animations had
+                            // all finished. This avoid re-laying out the Row with animated width,
+                            // which is _sometimes_ desirable. Try removing this modifier and
+                            // observe the effect.
+                            // .skipToLookaheadSize()
+                    ) {
+                        Icon(
+                            Icons.Outlined.Share,
+                            contentDescription = "Share",
+                            modifier = Modifier.padding(
+                                top = 10.dp,
+                                bottom = 10.dp,
+                                start = 10.dp,
+                                end = 20.dp
+                            )
+                        )
+                        Icon(
+                            Icons.Outlined.Favorite,
+                            contentDescription = "Favorite",
+                            modifier = Modifier.padding(
+                                top = 10.dp,
+                                bottom = 10.dp,
+                                start = 10.dp,
+                                end = 20.dp
+                            )
+                        )
+                        Icon(
+                            Icons.Outlined.Create,
+                            contentDescription = "Create",
+                            tint = Color.White,
+                            modifier = Modifier
+                                .sharedBounds(
+                                    rememberSharedContentState(key = "icon_background"),
+                                    this@AnimatedVisibility
+                                )
+                                .background(selectionColor, RoundedCornerShape(50))
+                                .padding(
+                                    top = 10.dp,
+                                    bottom = 10.dp,
+                                    start = 20.dp,
+                                    end = 20.dp
+                                )
+                                .sharedElement(
+                                    rememberSharedContentState(key = "icon"),
+                                    this@AnimatedVisibility
+                                )
+                        )
+                    }
+                }
+            }
+        }
+        AnimatedVisibility(
+            visible = !expanded,
+            enter = EnterTransition.None,
+            exit = ExitTransition.None
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Surface(
+                    Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(30.dp)
+                        .sharedBounds(
+                            rememberSharedContentState(key = "container"),
+                            this@AnimatedVisibility,
+                            enter = EnterTransition.None,
+                        )
+                        .sharedBounds(
+                            rememberSharedContentState(key = "icon_background"),
+                            this@AnimatedVisibility,
+                            enter = EnterTransition.None,
+                            exit = ExitTransition.None
+                        ),
+                    shape = RoundedCornerShape(30.dp),
+                    color = selectionColor
+                ) {
+                    Icon(
+                        Icons.Outlined.Create,
+                        contentDescription = "Create",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .padding(30.dp)
+                            .size(40.dp)
+                            .sharedElement(
+                                rememberSharedContentState(key = "icon"),
+                                this@AnimatedVisibility
+                            )
+                    )
+                }
+            }
+        }
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-val snapshotVersion : String? = System.getenv("COMPOSE_SNAPSHOT_ID")
+val snapshotVersion : String? = "11670047"//System.getenv("COMPOSE_SNAPSHOT_ID")
 
 pluginManagement {
     repositories {
@@ -10,9 +10,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven {
-            setUrl("/users/riggaroo/androidx-main/out/androidx/build/support_repo/")
-        }
         snapshotVersion?.let {
             println("https://androidx.dev/snapshots/builds/$it/artifacts/repository/")
             maven { url = uri("https://androidx.dev/snapshots/builds/$it/artifacts/repository/") }
