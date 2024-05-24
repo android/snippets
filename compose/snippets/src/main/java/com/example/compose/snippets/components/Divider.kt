934749c95fc11cdb8ae936be634b3f1b9c1341eb
jakeroseman <jakeroseman@users.noreply.github.com>
2024-05-24 10:35:23 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/Divider.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/Divider.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.components
 
 import androidx.compose.foundation.layout.Arrangement
@@ -8,9 +24,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -49,7 +65,6 @@ fun HorizontalDividerExample() {
     }
 }
 // [END android_compose_components_horizontaldivider]
-
 
 @Preview
 // [START android_compose_components_verticaldivider]
