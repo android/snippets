a6cfa3946293840885faea6866738c34ccf1ea0d
Don Turner <donturner@google.com>
2024-08-20 01:31:12 +0100

    Snippet updates for type safe navigation APIs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
 
 apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
