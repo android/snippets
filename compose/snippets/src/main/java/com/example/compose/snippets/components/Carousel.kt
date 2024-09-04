19ace2627bf0f6039a3333e8d29940c0e043b82a
Rebecca Franks <riggaroo@google.com>
2024-09-04 11:23:52 +0100

    Add M3 Carousel examples

---
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/Carousel.kt
@@ -0,0 +1,111 @@
+package com.example.compose.snippets.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.HorizontalUncontainedCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.snippets.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+// [START android_compose_carousel_multi_browse_basic]
+@Composable
+private fun CarouselExamples_MultiBrowse() {
+    data class CarouselItem(
+        val id: Int,
+        @DrawableRes val imageResId: Int,
+        val contentDescription: String
+    )
+
+    val items = remember {
+        listOf(
+            CarouselItem(0, R.drawable.cupcake, "cupcake"),
+            CarouselItem(1, R.drawable.donut, "donut"),
+            CarouselItem(2, R.drawable.eclair, "eclair"),
+            CarouselItem(3, R.drawable.froyo, "froyo"),
+            CarouselItem(4, R.drawable.gingerbread, "gingerbread"),
+        )
+    }
+
+    HorizontalMultiBrowseCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        preferredItemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) { i ->
+        val item = items[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(id = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+// [END android_compose_carousel_multi_browse_basic]
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+// [START android_compose_carousel_uncontained_basic]
+@Composable
+private fun CarouselExamples() {
+    data class CarouselItem(
+        val id: Int,
+        @DrawableRes val imageResId: Int,
+        val contentDescription: String
+    )
+
+    val items = remember {
+        listOf(
+            CarouselItem(0, R.drawable.cupcake, "cupcake"),
+            CarouselItem(1, R.drawable.donut, "donut"),
+            CarouselItem(2, R.drawable.eclair, "eclair"),
+            CarouselItem(3, R.drawable.froyo, "froyo"),
+            CarouselItem(4, R.drawable.gingerbread, "gingerbread"),
+        )
+    }
+
+    HorizontalUncontainedCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, bottom = 16.dp),
+        itemWidth = 186.dp,
+        itemSpacing = 8.dp,
+        contentPadding = PaddingValues(horizontal = 16.dp)
+    ) { i ->
+        val item = items[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(id = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+// [END android_compose_carousel_uncontained_basic]
+
