03147be3f96d8b7a261d9174cf766904b98c9db8
dmail <dmail@google.com>
2024-09-10 13:29:43 -0700

    First (pilot) commit for getting this phase of the snippets in.
    Adds the two snippets that were created for:
    https://developer.android.com/quick-guides/content/auto-format-phone-number?hl=en

--- compose/snippets/src/main/java/com/example/compose/snippets/text/TextSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/text/TextSnippets.kt
@@ -765,6 +765,74 @@ fun BasicMarqueeSample() {
 }
 // [END android_compose_text_marquee]
 
+
+// [START android_compose_text_auto_format_phone-number_textfieldconfig]
+@Composable
+fun PhoneNumber() {
+    var phoneNumber by rememberSaveable { mutableStateOf("") }
+    val numericRegex = Regex("[^0-9]")
+    TextField(
+        value = phoneNumber,
+        onValueChange = {
+            // Remove non-numeric characters.
+            val stripped = numericRegex.replace(it, "")
+            phoneNumber = if (stripped.length >= 10) {
+                stripped.substring(0..9)
+            } else {
+                stripped
+            }
+        },
+        label = { Text("Enter Phone Number") },
+        visualTransformation = NanpVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
+// [END android_compose_text_auto_format_phone-number_textfieldconfig]
+
+// [START android_compose_text_auto_format_phone-number_transformtext]
+class NanpVisualTransformation() : VisualTransformation {
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val trimmed = if (text.text.length >= 10) text.text.substring(0..9) else text.text
+
+        var out = if (trimmed.isNotEmpty()) "(" else ""
+
+        for (i in trimmed.indices) {
+            if (i == 3) out += ") "
+            if (i == 6) out += "-"
+            out += trimmed[i]
+        }
+        return TransformedText(AnnotatedString(out), phoneNumberOffsetTranslator)
+    }
+
+    private val phoneNumberOffsetTranslator = object : OffsetMapping {
+
+        override fun originalToTransformed(offset: Int): Int =
+            when (offset) {
+                0  -> offset
+                // Add 1 for opening parenthesis.
+                in 1..3 -> offset + 1
+                // Add 3 for both parentheses and a space.
+                in 4..6 -> offset + 3
+                // Add 4 for both parentheses, space, and hyphen.
+                else -> offset + 4
+            }
+
+        override fun transformedToOriginal(offset: Int): Int =
+            when (offset) {
+                0 -> offset
+                // Subtract 1 for opening parenthesis.
+                in 1..5 -> offset - 1
+                // Subtract 3 for both parentheses and a space.
+                in 6..10 -> offset - 3
+                // Subtract 4 for both parentheses, space, and hyphen.
+                else -> offset - 4
+            }
+
+    }
+}
+// [END android_compose_text_auto_format_phone-number_transformtext]
+
 private val firaSansFamily = FontFamily()
 
 val LightBlue = Color(0xFF0066FF)
