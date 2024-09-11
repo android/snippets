e1631c044d49278ff328bddc4f9b2d9c5ed1de64
dmail <dmail@google.com>
2024-09-11 15:40:25 -0700

    Adds snippet for the "Create a parallax scrolling effect" doc at https://developer.android.com/quick-guides/content/parallax-scrolling?hl=en

--- compose/snippets/src/main/java/com/example/compose/snippets/images/ParallaxEffect.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/images/ParallaxEffect.kt
@@ -1,2 +1,59 @@
 package com.example.compose.snippets.images
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.compose.snippets.R
+
+// [START android_compose_images_parallax]
+@Composable
+fun ParallaxEffect()
+{
+    fun Modifier.parallaxLayoutModifier(scrollState: ScrollState, rate: Int) =
+        layout { measurable, constraints ->
+            val placeable = measurable.measure(constraints)
+            val height = if (rate > 0) scrollState.value / rate else scrollState.value
+            layout(placeable.width, placeable.height) {
+                placeable.place(0, height)
+            }
+        }
+
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(scrollState),
+    ) {
+
+        Image(
+            painterResource(id = R.drawable.cupcake),
+            contentDescription = "Android logo",
+            contentScale = ContentScale.Fit,
+            // Reduce scrolling rate by half.
+            modifier = Modifier.parallaxLayoutModifier(scrollState, 2)
+        )
+
+        Text(
+            text = stringResource(R.string.detail_placeholder),
+            modifier = Modifier
+                .background(Color.White)
+                .padding(horizontal = 8.dp),
+
+            )
+    }
+}
+// [END android_compose_images_parallax]
