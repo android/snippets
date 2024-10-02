72a9fca88f040ecd1f3c5fcddb09b155ee7add81
Rebecca Franks <riggaroo@google.com>
2024-10-02 09:46:40 +0100

    Clean up landing screens, using Scaffold and list items.

--- compose/snippets/src/main/java/com/example/compose/snippets/components/ComponentsScreen.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/ComponentsScreen.kt
@@ -16,45 +16,63 @@
 
 package com.example.compose.snippets.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.compose.snippets.navigation.TopComponentsDestination
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComponentsScreen(
     navigate: (TopComponentsDestination) -> Unit
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        items(TopComponentsDestination.entries) { destination ->
-            NavigationItem(destination) {
-                navigate(
-                    destination
-                )
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text("Common Components")
+        })
+    }, content = { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            items(TopComponentsDestination.entries) { destination ->
+                NavigationItem(destination) {
+                    navigate(
+                        destination
+                    )
+                }
             }
         }
-    }
+    })
+
 }
 
 @Composable
 fun NavigationItem(destination: TopComponentsDestination, onClick: () -> Unit) {
-    Button(
-        onClick = { onClick() }
-    ) {
-        Text(destination.title)
-    }
+    ListItem(
+        headlineContent = {
+           Text(destination.title)
+        },
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    )
 }
