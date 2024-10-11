56e88c8bde975ef26dbde8b5286d2f9f21f2d0f0
jakeroseman <jakeroseman@google.com>
2024-10-11 10:15:38 +0100

    Remove unneeded dependencies

--- compose/snippets/build.gradle.kts
+++ compose/snippets/build.gradle.kts
@@ -77,7 +77,6 @@ android {
 
 dependencies {
     implementation(libs.androidx.work.runtime.ktx)
-    implementation(libs.androidx.core)
     val composeBom = platform(libs.androidx.compose.bom)
     implementation(composeBom)
     androidTestImplementation(composeBom)
