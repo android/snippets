f4330ceebd06fe6ba15ca78cfee6843395eb3c36
jakeroseman <jakeroseman@users.noreply.github.com>
2024-10-14 14:19:04 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/NavigationDrawer.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/NavigationDrawer.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -50,7 +66,7 @@ fun DetailedDrawerExample(
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
-    ModalNavigationDrawer (
+    ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
                 Spacer(Modifier.height(12.dp))
@@ -95,9 +111,9 @@ fun DetailedDrawerExample(
                     navigationIcon = {
                         IconButton(onClick = {
                             scope.launch {
-                                if (drawerState.isClosed){
+                                if (drawerState.isClosed) {
                                     drawerState.open()
-                                }else{
+                                } else {
                                     drawerState.close()
                                 }
                             }
