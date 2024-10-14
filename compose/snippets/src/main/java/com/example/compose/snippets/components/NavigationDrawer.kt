452e51600ff98651c7b793ab92e7460f2f866a33
jakeroseman <jakeroseman@google.com>
2024-10-14 14:51:04 +0100

    Fixing imports

--- compose/snippets/src/main/java/com/example/compose/snippets/components/NavigationDrawer.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/NavigationDrawer.kt
@@ -3,8 +3,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Help
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.DismissibleDrawerSheet
+import androidx.compose.material3.DismissibleNavigationDrawer
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -78,7 +91,11 @@ fun DismissibleDrawerExample(
                     navigationIcon = {
                         IconButton(onClick = {
                             scope.launch {
-                                drawerState.open()
+                                if (drawerState.isClosed){
+                                    drawerState.open()
+                                }else{
+                                    drawerState.close()
+                                }
                             }
                         }) {
                             Icon(Icons.Default.Menu, contentDescription = "Menu")
