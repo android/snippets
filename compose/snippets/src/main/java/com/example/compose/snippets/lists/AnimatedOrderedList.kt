c80e4dfb8ddeb5652a4456b56ab6313b48176173
jakeroseman <jakeroseman@users.noreply.github.com>
2024-10-15 13:30:49 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/lists/AnimatedOrderedList.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/lists/AnimatedOrderedList.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.lists
 
 import androidx.compose.foundation.layout.Arrangement
@@ -130,10 +146,12 @@ private fun OrderButtons(
                         selectedIndex = index
                         when (options[selectedIndex]) {
                             "Reset" -> displayedItems.value = data
-                            "Alphabetical" -> displayedItems.value =
-                                displayedItems.value.sortedWith(sortAlpha)
-                            "Length" -> displayedItems.value =
-                                displayedItems.value.sortedWith(sortLength)
+                            "Alphabetical" ->
+                                displayedItems.value =
+                                    displayedItems.value.sortedWith(sortAlpha)
+                            "Length" ->
+                                displayedItems.value =
+                                    displayedItems.value.sortedWith(sortLength)
                         }
                     },
                     selected = index == selectedIndex
@@ -148,7 +166,7 @@ private fun OrderButtons(
 
 @Preview
 @Composable
-fun ListAnimatingItemsExamplePreview(){
+fun ListAnimatingItemsExamplePreview() {
     val list = listOf("One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten")
     ListAnimatedItemsExample(list)
 }
