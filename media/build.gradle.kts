3bcd03fe02837c4265299d42e299ca2745e0fee0
Jolanda Verhoef <JolandaVerhoef@users.noreply.github.com>
2024-10-16 08:28:35 +0100

    Add basic Camera sample

---
+++ media/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.example.media"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.media"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    // Add CameraX Compose dependency
+    implementation(libs.androidx.camera.compose)
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.camera.lifecycle)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
---
+++ media/src/main/java/com/example/media/camera/CameraSnippets.kt
@@ -0,0 +1,89 @@
+package com.example.media.camera
+
+import android.content.Context
+import androidx.camera.compose.CameraXViewfinder
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceRequest
+import androidx.camera.core.UseCaseGroup
+import androidx.camera.core.takePicture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.lifecycle.awaitInstance
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.LifecycleStartEffect
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class CameraPreviewViewModel(private val appContext: Context): ViewModel() {
+    private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+    val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest
+
+    private lateinit var processCameraProvider: ProcessCameraProvider
+    private val previewUseCase = Preview.Builder().build()
+    private val captureUseCase = ImageCapture.Builder().build()
+    private val useCaseGroup = UseCaseGroup.Builder().apply {
+        addUseCase(previewUseCase)
+        addUseCase(captureUseCase)
+    }.build()
+
+    private var runningCameraJob: Job? = null
+
+    fun startCamera() {
+        stopCamera()
+        runningCameraJob = viewModelScope.launch {
+            processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
+            processCameraProvider.runWith(CameraSelector.DEFAULT_BACK_CAMERA, useCaseGroup) {
+                previewUseCase.setSurfaceProvider { newSurfaceRequest ->
+                    _surfaceRequest.value = newSurfaceRequest
+                }
+            }
+        }
+    }
+
+    fun stopCamera() {
+        runningCameraJob?.apply {
+            if (isActive) {
+                cancel()
+            }
+        }
+    }
+
+    fun takePicture() {
+        viewModelScope.launch {
+            val imageProxy = captureUseCase.takePicture()
+            // Do something with the image
+        }
+    }
+}
+
+@Composable
+fun CameraPreviewScreen(modifier: Modifier = Modifier) {
+    val viewModel = CameraPreviewViewModel(LocalContext.current.applicationContext)
+    val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
+
+    LifecycleStartEffect(Unit) {
+        viewModel.startCamera()
+        onStopOrDispose {
+            viewModel.stopCamera()
+        }
+    }
+
+    surfaceRequest?.let { CameraPreview(modifier, it) }
+}
+
+@Composable
+private fun CameraPreview(
+    modifier: Modifier = Modifier,
+    surfaceRequest: SurfaceRequest
+) {
+    CameraXViewfinder(surfaceRequest, modifier)
+}
