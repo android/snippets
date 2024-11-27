9987d5204874391a7012fd04ad5281f4931a3d79
jakeroseman <jakeroseman@google.com>
2024-11-27 20:33:17 +0000

    Add live search with FilterTextView example

---
+++ compose/snippets/src/main/java/com/example/compose/snippets/text/FilterText.kt
@@ -0,0 +1,90 @@
+package com.example.compose.snippets.text
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+// [START android_compose_text_filtertextviewmodel]
+class FilterTextViewModel : ViewModel() {
+    private val items = listOf(
+        "Cupcake",
+        "Donut",
+        "Eclair",
+        "Froyo",
+        "Gingerbread",
+        "Honeycomb",
+        "Ice Cream Sandwich"
+    )
+
+    private val _filteredItems = MutableStateFlow(items)
+    var filteredItems: StateFlow<List<String>> = _filteredItems
+
+    fun filterText(input: String) {
+        // This filter returns the full items list when input is an empty string.
+        _filteredItems.value = items.filter { it.contains(input, ignoreCase = true) }
+    }
+}
+// [END android_compose_text_filtertextviewmodel]
+
+// [START android_compose_text_filtertextview]
+@Composable
+fun FilterTextView(modifier: Modifier = Modifier, viewModel: FilterTextViewModel = viewModel()) {
+    val filteredItems by viewModel.filteredItems.collectAsStateWithLifecycle()
+    var text by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 10.dp)
+    ) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                viewModel.filterText(text)
+            },
+            label = { Text("Filter Text") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        LazyColumn {
+            items(
+                count = filteredItems.size,
+                key = { index -> filteredItems[index] }
+            ) {
+                ListItem(
+                    headlineContent = { Text(filteredItems[it]) },
+                    modifier = Modifier
+                        .fillParentMaxWidth()
+                        .padding(10.dp)
+                )
+            }
+        }
+    }
+}
+// [END android_compose_text_filtertextview]
+
+@Preview(showBackground = true)
+@Composable
+private fun FilterTextViewPreview() {
+    FilterTextView()
+}
