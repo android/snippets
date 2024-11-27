fb654a90218dec10d36fc058b7350505f391bc30
Michael Stillwell <mjs@beebo.org>
2024-11-27 13:45:18 +0000

    Init Wear Tiles snippets (#400)
    * Init Wear Tiles snippets
    
    * Add snippet markers
    
    * Add manifest snippet markers
    
    * Rename manifest snippet markers

---
+++ wear/src/main/java/com/example/wear/snippets/tile/Tile.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.wear.snippets.tile
+
+import androidx.wear.protolayout.ColorBuilders.argb
+import androidx.wear.protolayout.ResourceBuilders.Resources
+import androidx.wear.protolayout.TimelineBuilders.Timeline
+import androidx.wear.protolayout.material.Text
+import androidx.wear.protolayout.material.Typography
+import androidx.wear.tiles.RequestBuilders
+import androidx.wear.tiles.RequestBuilders.ResourcesRequest
+import androidx.wear.tiles.TileBuilders.Tile
+import androidx.wear.tiles.TileService
+import com.google.common.util.concurrent.Futures
+
+private const val RESOURCES_VERSION = "1"
+
+// [START android_wear_tile_mytileservice]
+class MyTileService : TileService() {
+
+    override fun onTileRequest(requestParams: RequestBuilders.TileRequest) =
+        Futures.immediateFuture(Tile.Builder()
+            .setResourcesVersion(RESOURCES_VERSION)
+            .setTileTimeline(
+                Timeline.fromLayoutElement(
+                    Text.Builder(this, "Hello World!")
+                        .setTypography(Typography.TYPOGRAPHY_BODY1)
+                        .setColor(argb(0xFFFFFFFF.toInt()))
+                        .build()))
+            .build())
+
+    override fun onTileResourcesRequest(requestParams: ResourcesRequest) =
+        Futures.immediateFuture(Resources.Builder()
+            .setVersion(RESOURCES_VERSION)
+            .build()
+        )
+
+}
+// [END android_wear_tile_mytileservice]
