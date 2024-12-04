b4619e3d44bdea2b6868e6b273c24e94a85e8f2a
jakeroseman <jakeroseman@users.noreply.github.com>
2024-12-04 19:29:30 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/SearchBar.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/SearchBar.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.components
 
 import androidx.compose.foundation.clickable
@@ -9,6 +25,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
@@ -36,7 +53,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.rememberScrollState
 
 @OptIn(ExperimentalMaterial3Api::class)
 // [START android_compose_components_searchbarbasicfilterlist]
@@ -47,8 +63,8 @@ fun SearchBarBasicFilterList(modifier: Modifier = Modifier) {
     Box(
         modifier
             .fillMaxSize()
-            .semantics { isTraversalGroup = true })
-    {
+            .semantics { isTraversalGroup = true }
+    ) {
         SearchBar(
             modifier = Modifier
                 .align(Alignment.TopCenter)
@@ -110,7 +126,8 @@ fun SearchBarFilterList(
     Box(
         modifier
             .fillMaxSize()
-            .semantics { isTraversalGroup = true }) {
+            .semantics { isTraversalGroup = true }
+    ) {
         SearchBar(
             modifier = Modifier
                 .align(Alignment.TopCenter)
@@ -197,4 +214,3 @@ fun AppSearchBar(modifier: Modifier = Modifier) {
         modifier
     )
 }
-
