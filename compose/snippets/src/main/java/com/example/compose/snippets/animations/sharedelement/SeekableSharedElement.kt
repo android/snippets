f4c131af991af153a52f77790e66e05fb38bcf61
Rebecca Franks <riggaroo@google.com>
2024-12-10 11:36:02 +0000

    Delete SeekableSharedElement.kt

--- compose/snippets/src/main/java/com/example/compose/snippets/animations/sharedelement/SeekableSharedElement.kt
+++
@@ -1,8 +0,0 @@
-package com.example.compose.snippets.animations.sharedelement
-
-import androidx.compose.animation.core.SeekableTransitionState
-import androidx.compose.animation.core.rememberTransition
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
-
