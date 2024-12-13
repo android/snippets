4d7e4ab58cf5efb606db44f8fa505bbeb333b069
jakeroseman <jakeroseman@users.noreply.github.com>
2024-12-13 17:25:30 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/SearchBar.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/SearchBar.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.components
 
 import androidx.compose.foundation.clickable
@@ -15,6 +31,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -37,8 +54,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.material3.Button
 
 @Preview
 @Composable
@@ -56,10 +71,10 @@ fun SearchBarExamples() {
             "basic" -> SearchBarBasicFilterList()
             "advanced" -> AppSearchBar()
             else -> {
-                Button(onClick = { currentExample = "basic" }){
+                Button(onClick = { currentExample = "basic" }) {
                     Text("Basic search bar with filter")
                 }
-                Button(onClick = { currentExample = "advanced" }){
+                Button(onClick = { currentExample = "advanced" }) {
                     Text("Advanced search bar with filter")
                 }
             }
@@ -227,4 +242,3 @@ fun AppSearchBar(modifier: Modifier = Modifier) {
         modifier
     )
 }
-
