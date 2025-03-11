2ecf884011cfe70c6909cf732f5f76fe3eb3953b
Chiping Yeh <cpy@google.com>
2025-03-11 16:49:26 -0400

    Add initial project structure for credman snippets

---
+++ identity/credentialmanager/src/main/java/com/example/identity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.identity.credentialmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.identity.credentialmanager.ui.theme.SnippetsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
