22ab49a4dc6475cd97b572075ed5072c5e9e6cc5
JonEckenrode <112520815+JonEckenrode@users.noreply.github.com>
2025-04-08 18:41:37 +0000

    Apply Spotless

--- misc/src/androidTest/java/com/example/snippets/DeviceCompatibilityModeTestKotlinSnippets.kt
+++ misc/src/androidTest/java/com/example/snippets/DeviceCompatibilityModeTestKotlinSnippets.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets
 
 import android.os.Bundle
@@ -7,9 +23,9 @@ import org.junit.Assert.assertFalse
 import org.junit.Rule
 import org.junit.Test
 
-class DeviceCompatibilityModeTestKotlinSnippets: AppCompatActivity() {
+class DeviceCompatibilityModeTestKotlinSnippets : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
@@ -25,10 +41,9 @@ class DeviceCompatibilityModeTestKotlinSnippets: AppCompatActivity() {
     }
     // [END android_device_compatibility_mode_assert_isLetterboxed_kotlin]
 
-
     // Classes used by snippets.
 
-    class MainActivity: AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
         fun isLetterboxed(): Boolean {
             return true
--- misc/src/main/java/com/example/snippets/DeviceCompatibilityModeKotlinSnippets.kt
+++ misc/src/main/java/com/example/snippets/DeviceCompatibilityModeKotlinSnippets.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets
 
 import android.os.Build
@@ -6,15 +22,15 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.window.layout.WindowMetricsCalculator
 
-class DeviceCompatibilityModeKotlinSnippets: AppCompatActivity() {
+class DeviceCompatibilityModeKotlinSnippets : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
-      super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
     // [START android_device_compatibility_mode_isLetterboxed_kotlin]
-    fun isLetterboxed(activity: AppCompatActivity) : Boolean {
+    fun isLetterboxed(activity: AppCompatActivity): Boolean {
         if (isInMultiWindowMode) return false
 
         val wmc = WindowMetricsCalculator.getOrCreate()
@@ -30,5 +46,4 @@ class DeviceCompatibilityModeKotlinSnippets: AppCompatActivity() {
         }
     }
     // [END android_device_compatibility_mode_isLetterboxed_kotlin]
-
 }
