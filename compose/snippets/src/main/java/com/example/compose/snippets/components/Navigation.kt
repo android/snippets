0683086a267a5ead4513e53f2577dde070d6dd73
wardlauren <203715894+wardlauren@users.noreply.github.com>
2025-04-23 18:59:08 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/Navigation.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/Navigation.kt
@@ -1,37 +1,53 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Album
 import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material.icons.filled.PlaylistAddCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.material3.NavigationRail
-import androidx.compose.material3.NavigationRailItem
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.PrimaryTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
 fun SongsScreen(modifier: Modifier = Modifier) {
@@ -139,8 +155,8 @@ fun NavigationRailExample(modifier: Modifier = Modifier) {
     val startDestination = Destination.SONGS
     var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
-    Scaffold (modifier = modifier) { contentPadding ->
-        NavigationRail (modifier = Modifier.padding(contentPadding)) {
+    Scaffold(modifier = modifier) { contentPadding ->
+        NavigationRail(modifier = Modifier.padding(contentPadding)) {
             Destination.entries.forEachIndexed { index, destination ->
                 NavigationRailItem(
                     selected = selectedDestination == index,
@@ -172,7 +188,7 @@ fun NavigationTabExample(modifier: Modifier = Modifier) {
     val startDestination = Destination.SONGS
     var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
 
-    Scaffold (modifier = modifier) { contentPadding ->
+    Scaffold(modifier = modifier) { contentPadding ->
         PrimaryTabRow(selectedTabIndex = selectedDestination, modifier = Modifier.padding(contentPadding)) {
             Destination.entries.forEachIndexed { index, destination ->
                 Tab(
@@ -195,5 +211,3 @@ fun NavigationTabExample(modifier: Modifier = Modifier) {
     }
 }
 // [END android_compose_components_navigationtabexample]
-
-
