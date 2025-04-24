f5ad7b09e646f03daca23cae0564cd9b1be40eb8
dturner <873212+dturner@users.noreply.github.com>
2025-04-24 12:45:24 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/draganddrop/DragAndDropSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/draganddrop/DragAndDropSnippets.kt
@@ -24,7 +24,6 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.draganddrop.dragAndDropSource
 import androidx.compose.foundation.draganddrop.dragAndDropTarget
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
