088823cb936a8374352ef61b7b9982a886bb49e9
Dereck Bridie <devbridie@users.noreply.github.com>
2025-04-28 17:46:40 +0200

    Migrate last batch of XR snippets (#507)
    * Migrate "Add 3D models to your app" to snippets
    
    * Migrate "Check for spatial capabilities" to snippets
    
    * Migrate "Transition from Home Space to Full Space" to snippets

--- xr/src/main/java/com/example/xr/compose/SpatialCapabilities.kt
+++ xr/src/main/java/com/example/xr/compose/SpatialCapabilities.kt
@@ -17,7 +17,13 @@
 package com.example.xr.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.spatial.Subspace
+import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.layout.SubspaceModifier
+import androidx.xr.compose.subspace.layout.fillMaxHeight
+import androidx.xr.compose.subspace.layout.width
 
 @Composable
 private fun SupportingInfoPanel() {}
@@ -38,3 +44,25 @@ private fun SpatialCapabilitiesCheck() {
     val spatialAudioEnabled = LocalSpatialCapabilities.current.isSpatialAudioEnabled
     // [END androidxr_compose_checkSpatialCapabilities]
 }
+
+@Composable
+private fun checkSpatialUiEnabled() {
+    // [START androidxr_compose_checkSpatialUiEnabled]
+    if (LocalSpatialCapabilities.current.isSpatialUiEnabled) {
+        Subspace {
+            SpatialPanel(
+                modifier = SubspaceModifier
+                    .width(1488.dp)
+                    .fillMaxHeight()
+            ) {
+                AppContent()
+            }
+        }
+    } else {
+        AppContent()
+    }
+    // [END androidxr_compose_checkSpatialUiEnabled]
+}
+
+@Composable
+private fun AppContent() {}
