2afa1cb4de5f7c2003aadea223a8501825caeb51
wardlauren <203715894+wardlauren@users.noreply.github.com>
2025-05-15 21:07:33 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/components/IconButton.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/IconButton.kt
@@ -21,9 +21,6 @@ import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
