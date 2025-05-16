fc4dbd47c317b263be461cb1cea1277373fa9d30
Meghan <magicalmeghan@google.com>
2025-05-16 18:25:43 -0400

    Update AutofillSnippets.kt

--- compose/snippets/src/main/java/com/example/compose/snippets/text/AutofillSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/text/AutofillSnippets.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.LocalAutofillHighlightColor
 import androidx.compose.foundation.text.input.rememberTextFieldState
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.TextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
