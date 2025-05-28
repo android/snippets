d14dd06afa0b592bc242f8362d60f8396ebc602e
Kevin Hufnagle <khufnagle@google.com>
2025-05-27 22:19:10 -0400

    Add code snippets for haptics in Android docs
    Provides a more robust and maintainable set of source code, compared to
    the manually-created snippets currently published on https://developer.android.com/develop/ui/views/haptics/custom-haptic-effects
    
    Also remove leftover test file

--- misc/src/main/java/com/example/snippets/ui/haptics/CustomVibrationCompositions.kt
+++ misc/src/main/java/com/example/snippets/ui/haptics/CustomVibrationCompositions.kt
@@ -1,52 +1,52 @@
-package com.example.snippets.ui.haptics;
+package com.example.snippets.ui.haptics
 
-import android.Manifest;
-import android.app.Activity;
-import android.os.Build;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
+import android.Manifest
+import android.app.Activity
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 
-import androidx.annotation.RequiresApi;
-import androidx.annotation.RequiresPermission;
+import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
 
 @RequiresApi(api = Build.VERSION_CODES.M)
-public class CustomVibrationPatternsJava extends Activity {
-    Vibrator vibrator = getApplicationContext().getSystemService(Vibrator.class);
+class CustomVibrationPatternsJava : Activity() {
+    val vibrator = applicationContext.getSystemService(Vibrator::class.java) as Vibrator
 
     // [START android_ui_haptics_ramp_up]
     @RequiresApi(Build.VERSION_CODES.O)
     @RequiresPermission(Manifest.permission.VIBRATE)
-    private void rampUpPattern(Vibrator vibrator) {
-        long[] timings = new long[] {
-                50, 50, 50, 50, 50, 100, 350, 25, 25, 25, 25, 200 };
-        int[] amplitudes = new int[] {
-                33, 51, 75, 113, 170, 255, 0, 38, 62, 100, 160, 255 };
-        int repeatIndex = -1; // Don't repeat.
+    fun rampUpPattern() {
+        val timings = longArrayOf(
+                50, 50, 50, 50, 50, 100, 350, 25, 25, 25, 25, 200)
+        val amplitudes = intArrayOf(
+                33, 51, 75, 113, 170, 255, 0, 38, 62, 100, 160, 255)
+        val repeatIndex = -1 // Don't repeat.
 
         vibrator.vibrate(VibrationEffect.createWaveform(
-                timings, amplitudes, repeatIndex));
+                timings, amplitudes, repeatIndex))
     }
     // [END android_ui_haptics_ramp_up]
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @RequiresPermission(Manifest.permission.VIBRATE)
     // [START android_ui_haptics_repeat]
-    private void startVibrating() {
-        long[] timings = new long[] { 50, 50, 100, 50, 50 };
-        int[] amplitudes = new int[] { 64, 128, 255, 128, 64 };
-        int repeat = 1; // Repeat from the second entry, index = 1.
+    fun startVibrating() {
+        val timings = longArrayOf(50, 50, 100, 50, 50)
+        val amplitudes = intArrayOf(64, 128, 255, 128, 64)
+        val repeat = 1 // Repeat from the second entry, index = 1.
         VibrationEffect repeatingEffect = VibrationEffect.createWaveform(
-                timings, amplitudes, repeat);
+                timings, amplitudes, repeat)
             // repeatingEffect can be used in multiple places.
 
-        vibrator.vibrate(repeatingEffect);
+        vibrator.vibrate(repeatingEffect)
     }
 
     // [START_EXCLUDE]
     @RequiresPermission(Manifest.permission.VIBRATE)
     // [END_EXCLUDE]
-    private void stopVibrating() {
-        vibrator.cancel();
+    fun stopVibrating() {
+        vibrator.cancel()
     }
     // [END android_ui_haptics_repeat]
 
@@ -54,18 +54,18 @@ public class CustomVibrationPatternsJava extends Activity {
     // [START android_ui_haptics_fallback]
     @RequiresApi(api = Build.VERSION_CODES.O)
     @RequiresPermission(Manifest.permission.VIBRATE)
-    private void patternWithFallback() {
-        long[] smoothTimings = new long[] { 50, 50, 100, 50, 50 };
-        long[] onOffTimings = new long[] { 50, 100 };
-        int[] amplitudes = new int[] { 64, 128, 255, 128, 64 };
-        int repeatIndex = -1; // Don't repeat.
+    fun patternWithFallback() {
+        val smoothTimings = longArrayOf(50, 50, 100, 50, 50)
+        val onOffTimings = longArrayOf(50, 100)
+        val amplitudes = intArrayOf(64, 128, 255, 128, 64)
+        val repeatIndex = -1 // Don't repeat.
 
         if (vibrator.hasAmplitudeControl()) {
             vibrator.vibrate(VibrationEffect.createWaveform(
-                    smoothTimings, amplitudes, repeatIndex));
+                    smoothTimings, amplitudes, repeatIndex))
         } else {
             vibrator.vibrate(VibrationEffect.createWaveform(
-                    onOffTimings, repeatIndex));
+                    onOffTimings, repeatIndex))
         }
     }
     // [END android_ui_haptics_fallback]
