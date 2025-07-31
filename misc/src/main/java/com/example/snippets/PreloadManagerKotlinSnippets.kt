07439af5fa979539b68982a5191e3b3fa781d55b
asolovay <149095394+asolovay@users.noreply.github.com>
2025-07-31 22:05:05 +0000

    Apply Spotless

--- misc/src/main/java/com/example/snippets/PreloadManagerKotlinSnippets.kt
+++ misc/src/main/java/com/example/snippets/PreloadManagerKotlinSnippets.kt
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets
 
-import androidx.media3.common.C
-import androidx.media3.exoplayer.source.preload.TargetPreloadStatusControl
 import android.os.Bundle
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.preload.DefaultPreloadManager
+import androidx.media3.exoplayer.source.preload.TargetPreloadStatusControl
 import java.lang.Math.abs
-
 
 // constants to make the code snippets work
 const val currentPlayingIndex = 10
@@ -71,7 +86,6 @@ class PreloadManagerSnippetsKotlin {
             // [START android_defaultpreloadmanager_invalidate]
             preloadManager.invalidate()
             // [END  android_defaultpreloadmanager_invalidate]
-
         }
 
         @OptIn(UnstableApi::class)
@@ -103,7 +117,6 @@ class PreloadManagerSnippetsKotlin {
             // [START android_defaultpreloadmanager_removeItem]
             preloadManager.remove(mediaItem)
             // [END android_defaultpreloadmanager_removeItem]
-
         }
 
         @OptIn(UnstableApi::class)
@@ -118,6 +131,4 @@ class PreloadManagerSnippetsKotlin {
             return listOf()
         }
     }
-
-
 }
