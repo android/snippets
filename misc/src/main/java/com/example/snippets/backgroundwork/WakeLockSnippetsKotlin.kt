5d53f2eaed43cc57483f30baf38b83fb523de429
asolovay <149095394+asolovay@users.noreply.github.com>
2025-08-07 21:12:05 +0000

    Apply Spotless

--- misc/src/main/java/com/example/snippets/backgroundwork/WakeLockSnippetsKotlin.kt
+++ misc/src/main/java/com/example/snippets/backgroundwork/WakeLockSnippetsKotlin.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets.backgroundwork
 
 import android.app.Activity
@@ -14,12 +30,10 @@ class WakeLockSnippetsKotlin : Activity() {
             newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyClassName::MyWakelockTag").apply {
                 acquire()
             }
-
         }
     // [END android_backgroundwork_wakelock_create_kotlin]
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
 
         super.onCreate(savedInstanceState)
     }
@@ -39,7 +53,5 @@ class WakeLockSnippetsKotlin : Activity() {
     // [END android_backgroundwork_wakelock_release_kotlin]
 
     private fun doTheWork() {
-
     }
-
 }
