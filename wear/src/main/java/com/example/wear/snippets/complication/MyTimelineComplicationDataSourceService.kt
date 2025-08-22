cd3cbb346a4a972f216e83defeeaed683af62ae5
garanj <25058277+garanj@users.noreply.github.com>
2025-08-22 12:56:48 +0000

    Apply Spotless

--- wear/src/main/java/com/example/wear/snippets/complication/MyTimelineComplicationDataSourceService.kt
+++ wear/src/main/java/com/example/wear/snippets/complication/MyTimelineComplicationDataSourceService.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.wear.snippets.complication
 
 import androidx.wear.watchface.complications.data.ComplicationData
