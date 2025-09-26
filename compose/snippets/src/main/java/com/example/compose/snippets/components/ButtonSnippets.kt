b03047f4215034d08e72232f32dddf04223a9d55
Aparna Kliebenstein <ashelke@.com>
2025-09-26 19:54:46 +0000

    feat: Add ButtonExample snippet
    Adds a new ButtonExample composable to the compose/snippets module.
    
    Region-Tag: android_compose_snippets_button_example

---
+++ compose/snippets/src/main/java/com/example/compose/snippets/components/ButtonSnippets.kt
@@ -0,0 +1,12 @@
+package com.example.compose.snippets.components
+
+import androidx.compose.runtime.Composable
+
+object ButtonSnippets {
+    // [START android_compose_snippets_button_example]
+    @Composable
+    fun ButtonExample() {
+
+    }
+    // [END android_compose_snippets_button_example]
+}
