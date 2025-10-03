0ce934934da194e7a03d1a0afb971a49f2cfa4cd
Dereck Bridie <devbridie@users.noreply.github.com>
2025-10-03 14:45:17 +0200

    Add incorrectly removed SessionConfigureConfigurationNotSupported (#638)

--- xr/src/main/java/com/example/xr/arcore/Hands.kt
+++ xr/src/main/java/com/example/xr/arcore/Hands.kt
@@ -23,6 +23,7 @@ import androidx.xr.arcore.Hand
 import androidx.xr.arcore.HandJointType
 import androidx.xr.runtime.Config
 import androidx.xr.runtime.Session
+import androidx.xr.runtime.SessionConfigureConfigurationNotSupported
 import androidx.xr.runtime.SessionConfigureSuccess
 import androidx.xr.runtime.math.Pose
 import androidx.xr.runtime.math.Quaternion
@@ -40,6 +41,8 @@ fun ComponentActivity.configureSession(session: Session) {
     )
     when (val result = session.configure(newConfig)) {
         is SessionConfigureSuccess -> TODO(/* Success! */)
+        is SessionConfigureConfigurationNotSupported ->
+            TODO(/* Some combinations of configurations are not valid. Handle this failure case. */)
         else ->
             TODO(/* The session could not be configured. See SessionConfigureResult for possible causes. */)
     }
