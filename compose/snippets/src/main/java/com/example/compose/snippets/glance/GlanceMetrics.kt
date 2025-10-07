69dcd215953d19e3b19891447e43f7a724411616
ashnohe <83780687+ashnohe@users.noreply.github.com>
2025-10-07 23:30:05 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/glance/GlanceMetrics.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/glance/GlanceMetrics.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.glance
 
 import android.appwidget.AppWidgetManager
@@ -9,7 +25,7 @@ private const val TAG = "WidgetMetrics"
 class GlanceMetrics {
 
     // [START android_compose_glance_metrics]
-    fun getWidgetEngagementMetrics(context: Context){
+    fun getWidgetEngagementMetrics(context: Context) {
         val manager = AppWidgetManager.getInstance(context)
 
         val endTime = System.currentTimeMillis()
@@ -27,7 +43,7 @@ class GlanceMetrics {
             "totalImpressionLength" to 0L
         )
 
-        for (event in events){
+        for (event in events) {
 
             Log.d(TAG, "Event Start: ${event.start}")
             Log.d(TAG, "Event End: ${event.end}")
@@ -40,7 +56,7 @@ class GlanceMetrics {
                 metrics["clicks"] = metrics.getValue("clicks") + clickedIds.size
                 // Log or analyze which components were clicked.
                 for (id in clickedIds) {
-                    Log.d(TAG,"Widget $widgetId: Tap event on component with ID $id")
+                    Log.d(TAG, "Widget $widgetId: Tap event on component with ID $id")
                 }
             }
 
@@ -56,15 +72,16 @@ class GlanceMetrics {
 
             // Impressions
             metrics["totalImpressionLength"] = metrics.getValue("totalImpressionLength") + event.visibleDuration.toMillis()
-            Log.d(TAG,
+            Log.d(
+                TAG,
                 "Widget $widgetId: Impression event with duration " + event.visibleDuration.toMillis() + "ms"
             )
-
 
             // Position
             val position = event.position
             if (position != null) {
-                Log.d(TAG,
+                Log.d(
+                    TAG,
                     "Widget $widgetId: left=${position.left}, right=${position.right}, top=${position.top}, bottom=${position.bottom}"
                 )
             }
