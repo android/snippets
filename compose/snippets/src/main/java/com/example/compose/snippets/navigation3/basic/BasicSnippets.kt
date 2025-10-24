9fb0e2df23c9bf4ffcb3bb12190416794e97b608
Srikrishna Sakunia <sakuniacoolkrishna636@gmail.com>
2025-10-24 18:06:27 +0100

    Update Nav3 to beta01

--- compose/snippets/src/main/java/com/example/compose/snippets/navigation3/basic/BasicSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/navigation3/basic/BasicSnippets.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.navigation3.runtime.NavEntry
-import androidx.navigation3.runtime.entry
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.example.compose.snippets.navigation3.ContentBlue
