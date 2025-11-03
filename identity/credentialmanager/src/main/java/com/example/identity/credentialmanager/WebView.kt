0b48e9ea60560f7eeb9b243d71c402241c517764
Chiping Yeh <cpy@google.com>
2025-11-03 13:17:08 -0500

    Add new WebView snippet for native implementation

---
+++ identity/credentialmanager/src/main/java/com/example/identity/credentialmanager/WebView.kt
@@ -0,0 +1,50 @@
+package com.example.identity.credentialmanager
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+import android.webkit.WebView
+import com.example.identity.WebViewClientImpl
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val url = "https://passkeys-codelab.glitch.me/"
+            AndroidView(
+                factory = { context ->
+                    WebView(context).apply {
+                        settings.javaScriptEnabled = true
+
+                        webViewClient = WebViewClientImpl()
+                    }
+                },
+                update = { webView ->
+                    run {
+                        webView.loadUrl(url)
+                        if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_AUTHENTICATION)) {
+                            WebSettingsCompat.setWebAuthenticationSupport(
+                                webView.settings,
+                                WebSettingsCompat.WEB_AUTHENTICATION_SUPPORT_FOR_APP,
+                            )
+                            // Check if getWebauthenticationSupport may have been disabled by the WebView.
+                            Log.e(
+                                "WebViewPasskeyDemo",
+                                "getWebAuthenticationSupport result: " + WebSettingsCompat.getWebAuthenticationSupport(
+                                    webView.settings
+                                ),
+                            )
+                        } else {
+                            Log.e("WebViewPasskeyDemo", "WebView does not support passkeys.")
+                        }
+                    }
+                },
+            )
+        }
+    }
+}
