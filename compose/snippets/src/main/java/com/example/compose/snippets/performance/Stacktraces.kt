a17610b41450568f119aa81e156cbf075fce6a5d
riggaroo <9973046+riggaroo@users.noreply.github.com>
2025-12-01 12:29:55 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/performance/Stacktraces.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/performance/Stacktraces.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.performance
 
 import android.app.Application
@@ -26,11 +42,13 @@ class SampleStackTracesEnabledApp : Application() {
 
     override fun onCreate() {
         // Enable only for debug flavor to avoid perf regressions in release
-        Composer.setDiagnosticStackTraceMode(if (BuildConfig.DEBUG) {
-            ComposeStackTraceMode.Auto
-        } else {
-            ComposeStackTraceMode.None
-        })
+        Composer.setDiagnosticStackTraceMode(
+            if (BuildConfig.DEBUG) {
+                ComposeStackTraceMode.Auto
+            } else {
+                ComposeStackTraceMode.None
+            }
+        )
     }
 }
 // [END android_compose_stacktraces_enable]
