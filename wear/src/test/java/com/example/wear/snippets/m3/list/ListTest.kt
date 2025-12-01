e658ad7db9daf748656d11c89097a4eb78ce7a7d
Chiara Chiappini <nunca.aqui@gmail.com>
2025-12-01 17:09:34 +0000

    Add snippet for screenshot test (#710)
    * Add snippet for screenshot test
    
    * Apply Spotless
    
    ---------
    
    Co-authored-by: kul3r4 <820891+kul3r4@users.noreply.github.com>

---
+++ wear/src/test/java/com/example/wear/snippets/m3/list/ListTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.wear.snippets.m3.list
+
+import com.google.android.horologist.compose.layout.AppScaffold
+import com.google.android.horologist.screenshots.rng.WearDevice
+import com.google.android.horologist.screenshots.rng.WearScreenshotTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+
+// [START android_wear_screenshot_test]
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class ComposeListScreenTest(
+    override val device: WearDevice
+) : WearScreenshotTest() {
+    override val tolerance = 0.02f
+
+    @Test
+    fun myScreenTest() =
+        runTest {
+            AppScaffold {
+                ComposeList()
+            }
+        }
+
+    companion object {
+        @JvmStatic
+        @ParameterizedRobolectricTestRunner.Parameters
+        fun devices() = WearDevice.entries
+    }
+}
+// [END android_wear_screenshot_test]
