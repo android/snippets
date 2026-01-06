680096b4d9d0c3819800d288e49c42e3e71d0d13
telpirion <7399197+telpirion@users.noreply.github.com>
2026-01-06 22:25:21 +0000

    Apply Spotless

--- datastore/src/main/java/com/example/datastore/snippets/sharedprefsmigration/PreferencesDataStore.kt
+++ datastore/src/main/java/com/example/datastore/snippets/sharedprefsmigration/PreferencesDataStore.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.datastore.snippets.sharedprefsmigration
 
 import android.content.Context
