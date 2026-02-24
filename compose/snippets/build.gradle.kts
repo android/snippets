876d124ef9b38359f009ced7aeba6d196d460ffe
Don Turner <donturner@google.com>
2026-02-24 23:09:43 +0000

    Add code snippets for FlexBox

--- compose/snippets/build.gradle.kts
+++ compose/snippets/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.util)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.graphics.shapes)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.ui.viewbinding)
--- compose/snippets/src/main/java/com/example/compose/snippets/ui/theme/Color.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/ui/theme/Color.kt
@@ -40,3 +40,14 @@ val PastelBlue = Color(0xFF9BF6FF)
 val PastelMauve = Color(0xFFA0C4FF)
 val PastelPurple = Color(0xFFBDB2FF)
 val PastelPink = Color(0xFFFFC6FF)
+
+val colors = listOf(
+    PastelRed,
+    PastelOrange,
+    PastelYellow,
+    PastelGreen,
+    PastelBlue,
+    PastelMauve,
+    PastelPurple,
+    PastelPink
+)
