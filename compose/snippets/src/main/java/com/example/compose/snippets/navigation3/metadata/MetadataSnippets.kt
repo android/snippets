2004c79c6cd96f342e55113cf60853c3fe9be3e5
Ben Sagmoe <bsagmoe@google.com>
2026-03-16 15:34:47 -0400

    Apply spotless fixes

--- compose/snippets/src/main/java/com/example/compose/snippets/navigation3/metadata/MetadataSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/navigation3/metadata/MetadataSnippets.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.compose.snippets.navigation3.metadata
 
 import androidx.compose.animation.AnimatedContentTransitionScope
@@ -15,7 +31,6 @@ import androidx.navigation3.runtime.metadata
 import androidx.navigation3.scene.Scene
 import androidx.navigation3.scene.SceneStrategy
 import androidx.navigation3.scene.SceneStrategyScope
-
 
 data object Home : NavKey
 data class Conversation(val id: String) : NavKey
@@ -61,7 +76,6 @@ public object NavDisplay {
 
     public object TransitionKey :
         NavMetadataKey<AnimatedContentTransitionScope<Scene<*>>.() -> ContentTransform>
-
 }
 // [END android_compose_navigation3_metadata_3]
 
