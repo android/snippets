40e0018d16fc12cf846aa9e6f4fe2b0cf74a1c89
kul3r4 <820891+kul3r4@users.noreply.github.com>
2026-04-02 16:43:30 +0000

    Apply Spotless

--- wear/src/main/java/com/example/wear/snippets/audio/RemoteMediaSession.kt
+++ wear/src/main/java/com/example/wear/snippets/audio/RemoteMediaSession.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.wear.snippets.audio
 
 import android.app.Application
@@ -10,6 +26,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
+import java.util.concurrent.Executor
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.channels.trySendBlocking
@@ -18,9 +35,7 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.mapNotNull
 import kotlinx.coroutines.guava.await
-import java.util.concurrent.Executor
 
 class RemoteMediaActivityViewModel(application: Application) : AndroidViewModel(application) {
     private val mediaSessionManager = application.getSystemService(MediaSessionManager::class.java)
