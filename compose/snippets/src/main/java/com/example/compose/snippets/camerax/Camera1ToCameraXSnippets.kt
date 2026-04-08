6c9d3f70675c496d10df4a55ca58becc0d611ea8
madebymozart <5649571+madebymozart@users.noreply.github.com>
2026-04-08 04:26:46 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/camerax/Camera1ToCameraXSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/camerax/Camera1ToCameraXSnippets.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.compose.snippets.camerax
 
 import android.content.Context
