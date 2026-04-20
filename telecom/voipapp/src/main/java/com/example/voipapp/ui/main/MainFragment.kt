b4420af50b71dbdf9250a5058603bdef379065ca
natarajkr <natarajkr@google.com>
2026-04-20 23:29:29 +0530

    Add Telecom Jetpack library snippets
    * Add snippets for VoIP call log exclusion
    * Add snippets for handling VoIP callback intents
    * Add snippets for dialer callback initiation
    * Add sample dialer and VoIP app modules for Telecom
    * Add core-telecom dependency and necessary permissions configuration

---
+++ telecom/voipapp/src/main/java/com/example/voipapp/ui/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.voipapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.voipapp.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
