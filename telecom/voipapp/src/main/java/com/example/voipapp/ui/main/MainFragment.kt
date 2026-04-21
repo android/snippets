e46d916ce1c1397fde49de5316d0ac39971cf542
natarajkr <natarajkr@google.com>
2026-04-21 15:29:21 +0530

    Add license headers and update formatting for telecom snippets
    * Add license headers to XML and Kotlin files in dialer and voipapp modules
    * Apply minor code formatting and cleanup to Kotlin files
    * Standardize XML declarations in resource files

--- telecom/voipapp/src/main/java/com/example/voipapp/ui/main/MainFragment.kt
+++ telecom/voipapp/src/main/java/com/example/voipapp/ui/main/MainFragment.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.voipapp.ui.main
 
 import android.os.Bundle
@@ -18,14 +34,13 @@ class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
-
 }
