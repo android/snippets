4352cbc6af5a23abe0015c98e43caf5a2bb1e0a2
natarajkr <natarajkr@google.com>
2026-04-21 15:29:21 +0530

    Add license headers and update formatting for telecom snippets
    * Add license headers to XML and Kotlin files in dialer and voipapp modules
    * Apply minor code formatting and cleanup to Kotlin files
    * Standardize XML declarations in resource files

--- telecom/voipapp/src/main/java/com/example/voipapp/VoipCallbackActivity.kt
+++ telecom/voipapp/src/main/java/com/example/voipapp/VoipCallbackActivity.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.voipapp
 
 import android.net.Uri
@@ -42,11 +58,13 @@ class VoipCallbackActivity : AppCompatActivity() {
                 CallAttributesCompat.DIRECTION_OUTGOING
             },
             callType = CallAttributesCompat.CALL_TYPE_AUDIO_CALL,
-            callCapabilities = (CallAttributesCompat.SUPPORTS_SET_INACTIVE
+            callCapabilities = (
+                CallAttributesCompat.SUPPORTS_SET_INACTIVE
                     or CallAttributesCompat.SUPPORTS_STREAM
-                    or CallAttributesCompat.SUPPORTS_TRANSFER),
+                    or CallAttributesCompat.SUPPORTS_TRANSFER
+                ),
         )
-        // END android_telecom_call_attributes_call_log_exclusion
+    // END android_telecom_call_attributes_call_log_exclusion
 
     @RequiresApi(Build.VERSION_CODES_FULL.BAKLAVA_1)
     private fun handleCallBack() {
