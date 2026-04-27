d3a1842fcbf44b8bfa35136523f7aeccad71fef8
Meghan <magicalmeghan@google.com>
2026-04-27 16:49:17 -0400

    Update dependencies

--- compose/snippets/src/androidTest/java/com/example/compose/snippets/interop/AddingComposeToYourAppSnippets.kt
+++ compose/snippets/src/androidTest/java/com/example/compose/snippets/interop/AddingComposeToYourAppSnippets.kt
@@ -17,7 +17,7 @@
 package com.example.compose.snippets.interop
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.compose.snippets.MyActivity
