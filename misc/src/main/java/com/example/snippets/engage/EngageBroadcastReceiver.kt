7463b4054cd3c4002ee25fc76af7836d9eef0366
huewu <1445837+huewu@users.noreply.github.com>
2026-05-12 06:22:27 +0000

    Apply Spotless

--- misc/src/main/java/com/example/snippets/engage/EngageBroadcastReceiver.kt
+++ misc/src/main/java/com/example/snippets/engage/EngageBroadcastReceiver.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets.engage
 
 // [START android_engage_broadcast_receiver_implementation]
@@ -18,17 +34,17 @@ class EngageBroadcastReceiver : BroadcastReceiver() {
         if (intent == null || context == null) return
         when (intent.action) {
             com.google.android.engage.service.Intents.ACTION_PUBLISH_RECOMMENDATION
-                -> EngagePublisher.publishOneTime(context,Constants.PUBLISH_TYPE_RECOMMENDATIONS)
+            -> EngagePublisher.publishOneTime(context, Constants.PUBLISH_TYPE_RECOMMENDATIONS)
             // Note: If app handles other publish actions (e.g. Featured, Continuation), add them here.
             com.google.android.engage.service.Intents.ACTION_PUBLISH_FEATURED
-                -> EngagePublisher.publishOneTime(context, Constants.PUBLISH_TYPE_FEATURED)
+            -> EngagePublisher.publishOneTime(context, Constants.PUBLISH_TYPE_FEATURED)
 
             /** Note: If vertical has other intents (e.g. FOOD shopping cart, etc.), add them here.
              * com.google.android.engage.food.service.Intents.ACTION_PUBLISH_FOOD_SHOPPING_CART
              * -> EngagePublisher.publishOneTime(context, Constants.PUBLISH_TYPE_FOOD_SHOPPING_CARD)
              * com.google.android.engage.travel.service.Intents.ACTION_PUBLISH_RESERVATION
              * -> EngagePublisher.publishOneTime(context, Constants.PUBLISH_TYPE_RESERVATION )
-            **/
+             **/
         }
     }
 
--- misc/src/main/java/com/example/snippets/engage/EngagePublisher.kt
+++ misc/src/main/java/com/example/snippets/engage/EngagePublisher.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.snippets.engage
 
 import android.content.Context
@@ -13,7 +29,7 @@ import java.util.concurrent.TimeUnit
 // [START android_engage_publisher_implementation]
 object EngagePublisher {
 
-    fun publishPeriodically(context: Context, publishType : String) {
+    fun publishPeriodically(context: Context, publishType: String) {
         val workRequest = PeriodicWorkRequestBuilder<EngageWorker>(Constants.REPEAT_INTERVAL, TimeUnit.HOURS)
             .setInputData(workDataOf(Constants.PUBLISH_TYPE_KEY to publishType))
             .build()
