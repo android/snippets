ffe85be77d40bb39bae158764bfcf6a582eed51f
sudeshim3 <34063388+sudeshim3@users.noreply.github.com>
2026-05-15 21:11:28 +0000

    Apply Spotless

--- misc/src/main/java/com/example/snippets/memoryleaks/MemoryLeakSnippets.kt
+++ misc/src/main/java/com/example/snippets/memoryleaks/MemoryLeakSnippets.kt
@@ -33,7 +33,6 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
