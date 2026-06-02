23114538be084506d8fe9bd3c5eae84da7e82727
sidd607 <siddartha.padhi@gmail.com>
2026-06-02 16:32:11 +0530

    updated build.gradle to build.gradle.kts

---
+++ playbilling/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+kotlin {
+    jvmToolchain(24)
+}
+
+android {
+    namespace = "com.example.pbl"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.pbl"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.guava)
+    implementation(libs.billing)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.android.material)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation("com.android.billingclient:billing-ktx:${libs.versions.playbilling.get()}")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
