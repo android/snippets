6659f10f092e3e1ef14752ab3a04cfd457b8f641
Siddartha Padhi <siddpadhi@google.com>
2026-06-02 14:52:23 +0530

    Kotlin code snippets for play billing library

---
+++ playbilling/src/main/java/com/example/pbl/kotlin/Errors.kt
@@ -0,0 +1,253 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.pbl.kotlin
+
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.QueryPurchasesParams
+import com.android.billingclient.api.PendingPurchasesParams
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+// [START android_playbilling_errors_simple_retry]
+class BillingClientWrapper(private val context: Context) : PurchasesUpdatedListener {
+  // Initialize the BillingClient.
+  private val billingClient = BillingClient.newBuilder(context)
+    .setListener(this)
+    .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
+    .build()
+
+  // Establish a connection to Google Play.
+  fun startBillingConnection() {
+    billingClient.startConnection(object : BillingClientStateListener {
+      override fun onBillingSetupFinished(billingResult: BillingResult) {
+        if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+          Log.d(TAG, "Billing response OK")
+          // The BillingClient is ready. You can now query Products Purchases.
+        } else {
+          Log.e(TAG, billingResult.debugMessage)
+          retryBillingServiceConnection()
+        }
+      }
+
+      override fun onBillingServiceDisconnected() {
+        Log.e(TAG, "GBPL Service disconnected")
+        retryBillingServiceConnection()
+      }
+    })
+  }
+
+  // Billing connection retry logic. This is a simple max retry pattern
+  private fun retryBillingServiceConnection() {
+    val maxTries = 3
+    var tries = 1
+    var isConnectionEstablished = false
+    do {
+      try {
+        billingClient.startConnection(object : BillingClientStateListener {
+          override fun onBillingSetupFinished(billingResult: BillingResult) {
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+              isConnectionEstablished = true
+              Log.d(TAG, "Billing connection retry succeeded.")
+            } else {
+              Log.e(
+                TAG,
+                "Billing connection retry failed: ${billingResult.debugMessage}"
+              )
+            }
+          }
+
+          override fun onBillingServiceDisconnected() {
+            // Retry logic or logging
+          }
+        })
+      } catch (e: Exception) {
+        e.message?.let { Log.e(TAG, it) }
+      } finally {
+        tries++
+      }
+    } while (tries <= maxTries && !isConnectionEstablished)
+  }
+  // ...
+// [END android_playbilling_errors_simple_retry]
+
+  companion object {
+      private const val TAG = "BillingClientWrapper"
+  }
+
+  override fun onPurchasesUpdated(billingResult: BillingResult, purchases: MutableList<Purchase>?) {
+      // Process purchases
+  }
+
+  // [START android_playbilling_errors_exponential_backoff]
+  private fun acknowledge(purchaseToken: String): BillingResult {
+    val params = AcknowledgePurchaseParams.newBuilder()
+      .setPurchaseToken(purchaseToken)
+      .build()
+    var ackResult = BillingResult()
+    billingClient.acknowledgePurchase(params) { billingResult ->
+      ackResult = billingResult
+    }
+    return ackResult
+  }
+
+  suspend fun acknowledgePurchase(purchaseToken: String) {
+
+    val retryDelayMs = 2000L
+    val retryFactor = 2
+    val maxTries = 3
+
+    withContext(Dispatchers.IO) {
+      acknowledge(purchaseToken)
+    }
+
+    AcknowledgePurchaseResponseListener { acknowledgePurchaseResult ->
+      val playBillingResponseCode =
+      PlayBillingResponseCode(acknowledgePurchaseResult.responseCode)
+      when (playBillingResponseCode) {
+        BillingClient.BillingResponseCode.OK -> {
+          Log.i(TAG, "Acknowledgement was successful")
+        }
+        BillingClient.BillingResponseCode.ITEM_NOT_OWNED -> {
+          // This is possibly related to a stale Play cache.
+          // Querying purchases again.
+          Log.d(TAG, "Acknowledgement failed with ITEM_NOT_OWNED")
+          billingClient.queryPurchasesAsync(
+            QueryPurchasesParams.newBuilder()
+              .setProductType(BillingClient.ProductType.SUBS)
+              .build()
+          )
+          { billingResult, purchaseList ->
+            when (billingResult.responseCode) {
+              BillingClient.BillingResponseCode.OK -> {
+                purchaseList.forEach { purchase ->
+                  acknowledge(purchase.purchaseToken)
+                }
+              }
+            }
+          }
+        }
+        in setOf(
+           BillingClient.BillingResponseCode.ERROR,
+           BillingClient.BillingResponseCode.SERVICE_DISCONNECTED,
+           BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE,
+         ) -> {
+          Log.d(
+            TAG,
+            "Acknowledgement failed, but can be retried -- " +
+            "Response Code: ${acknowledgePurchaseResult.responseCode} -- " +
+            "Debug Message: ${acknowledgePurchaseResult.debugMessage}"
+          )
+          runBlocking {
+            exponentialRetry(
+              maxTries = maxTries,
+              initialDelay = retryDelayMs,
+              retryFactor = retryFactor
+            ) { acknowledge(purchaseToken) }
+          }
+        }
+        in setOf(
+           BillingClient.BillingResponseCode.BILLING_UNAVAILABLE,
+           BillingClient.BillingResponseCode.DEVELOPER_ERROR,
+           BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED,
+         ) -> {
+          Log.e(
+            TAG,
+            "Acknowledgement failed and cannot be retried -- " +
+            "Response Code: ${acknowledgePurchaseResult.responseCode} -- " +
+            "Debug Message: ${acknowledgePurchaseResult.debugMessage}"
+          )
+          throw Exception("Failed to acknowledge the purchase!")
+        }
+      }
+    }
+  }
+
+  private suspend fun <T> exponentialRetry(
+    maxTries: Int = Int.MAX_VALUE,
+    initialDelay: Long = Long.MAX_VALUE,
+    retryFactor: Int = Int.MAX_VALUE,
+    block: suspend () -> T
+  ): T? {
+    var currentDelay = initialDelay
+    var retryAttempt = 1
+    do {
+      runCatching {
+        delay(currentDelay)
+        block()
+      }
+        .onSuccess {
+          Log.d(TAG, "Retry succeeded")
+          return@onSuccess
+        }
+        .onFailure { throwable ->
+          Log.e(
+            TAG,
+            "Retry Failed -- Cause: ${throwable.cause} -- Message: ${throwable.message}"
+          )
+        }
+      currentDelay *= retryFactor
+      retryAttempt++
+    } while (retryAttempt < maxTries)
+
+    return block() // last attempt
+  }
+  // [END android_playbilling_errors_exponential_backoff]
+
+  private fun dummy(listener: PurchasesUpdatedListener) {
+      // [START android_playbilling_errors_auto_reconnection]
+      val billingClient = BillingClient.newBuilder(context)
+          .setListener(listener)
+          .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
+          .enableAutoServiceReconnection() // Enable automatic service reconnection
+          .build()
+      // [END android_playbilling_errors_auto_reconnection]
+  }
+
+  private fun featureSupport(activity: Activity) {
+      val billingClient = ShadowBillingClient(this.billingClient)
+      // [START android_playbilling_errors_feature_support]
+      when {
+        billingClient.isReady -> {
+          if (billingClient.isFeatureSupported(BillingClient.FeatureType.IN_APP_MESSAGING)) {
+             // use feature
+          }
+        }
+      }
+      // [END android_playbilling_errors_feature_support]
+  }
+}
+
+// Helper mapping class/function
+private fun PlayBillingResponseCode(code: Int): Int = code
+
+private class ShadowBillingClient(val delegate: BillingClient) {
+    val isReady: Boolean get() = delegate.isReady
+    fun isFeatureSupported(feature: String): Boolean {
+        return delegate.isFeatureSupported(feature).responseCode == BillingClient.BillingResponseCode.OK
+    }
+}
---
+++ playbilling/src/main/java/com/example/pbl/kotlin/Integrate.kt
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.pbl.kotlin
+
+import android.content.Context
+import com.android.billingclient.api.AcknowledgePurchaseParams
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClient.BillingResponseCode
+import com.android.billingclient.api.BillingClient.ProductType
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingConfig
+import com.android.billingclient.api.BillingConfigResponseListener
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.ConsumeParams
+import com.android.billingclient.api.GetBillingConfigParams
+import com.android.billingclient.api.PendingPurchasesParams
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.PurchasesUpdatedListener
+import com.android.billingclient.api.QueryProductDetailsParams
+import com.android.billingclient.api.QueryProductDetailsParams.Product
+import com.android.billingclient.api.acknowledgePurchase
+import com.android.billingclient.api.consumePurchase
+import com.android.billingclient.api.queryProductDetails
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+private class Integrate(private val context: Context) : PurchasesUpdatedListener {
+
+    // [START android_playbilling_initialize_billing_client]
+    private val purchasesUpdatedListener =
+        PurchasesUpdatedListener { billingResult, purchases ->
+            // To be implemented in a later section.
+        }
+
+    private var billingClient = BillingClient.newBuilder(context)
+        .setListener(purchasesUpdatedListener)
+        // Configure other settings.
+        .build()
+    // [END android_playbilling_initialize_billing_client]
+
+    private fun connectToPlay() {
+        // [START android_playbilling_connect_to_play]
+        billingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                }
+            }
+            override fun onBillingServiceDisconnected() {
+                // If automatic service reconnection is enabled, this can be left empty (no-op)
+                // because the library handles retries. You can still use this for non-retry
+                // tasks like logging or updating the UI to reflect a disconnected state.
+                // Otherwise, try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        })
+        // [END android_playbilling_connect_to_play]
+    }
+
+    private fun enableAutoReconnection(listener: PurchasesUpdatedListener) {
+        // [START android_playbilling_enable_auto_reconnection]
+        val billingClient = BillingClient.newBuilder(context)
+            .setListener(listener)
+            .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
+            .enableAutoServiceReconnection() // Add this line to enable reconnection
+            .build()
+        // [END android_playbilling_enable_auto_reconnection]
+    }
+
+    private fun queryProductDetails() {
+        // [START android_playbilling_query_product_details]
+        val queryProductDetailsParams =
+            QueryProductDetailsParams.newBuilder()
+                .setProductList(
+                    listOf(
+                        Product.newBuilder()
+                            .setProductId("product_id_example")
+                            .setProductType(ProductType.SUBS)
+                            .build()
+                    )
+                )
+                .build()
+
+        billingClient.queryProductDetailsAsync(queryProductDetailsParams) {
+                billingResult,
+                queryProductDetailsResult ->
+            if (billingResult.responseCode == BillingResponseCode.OK) {
+                for (productDetails in queryProductDetailsResult.productDetailsList) {
+                    // Process successfully retrieved product details here.
+                }
+
+                for (unfetchedProduct in queryProductDetailsResult.unfetchedProductList) {
+                    // Handle any unfetched products as appropriate.
+                }
+            }
+        }
+        // [END android_playbilling_query_product_details]
+    }
+
+    // [START android_playbilling_query_kotlin_extensions]
+    suspend fun processPurchases() {
+        val productList = listOf(
+            QueryProductDetailsParams.Product.newBuilder()
+                .setProductId("product_id_example")
+                .setProductType(BillingClient.ProductType.SUBS)
+                .build()
+        )
+        val params = QueryProductDetailsParams.newBuilder()
+        params.setProductList(productList)
+
+        // leverage queryProductDetails Kotlin extension function
+        val productDetailsResult = withContext(Dispatchers.IO) {
+            billingClient.queryProductDetails(params.build())
+        }
+
+        // Process the result.
+    }
+    // [END android_playbilling_query_kotlin_extensions]
+
+    // [START android_playbilling_on_purchases_updated]
+    override fun onPurchasesUpdated(billingResult: BillingResult, purchases: List<Purchase>?) {
+        if (billingResult.responseCode == BillingResponseCode.OK && purchases != null) {
+            for (purchase in purchases) {
+                // Process the purchase as described in the next section.
+            }
+        } else if (billingResult.responseCode == BillingResponseCode.USER_CANCELED) {
+            // Handle an error caused by a user canceling the purchase flow.
+        } else {
+            // Handle any other error codes.
+        }
+    }
+    // [END android_playbilling_on_purchases_updated]
+
+    private suspend fun consumePurchase(purchase: Purchase, client: BillingClient) {
+        // [START android_playbilling_consume_purchase]
+        val consumeParams =
+            ConsumeParams.newBuilder()
+                .setPurchaseToken(purchase.getPurchaseToken())
+                .build()
+        val consumeResult = withContext(Dispatchers.IO) {
+            client.consumePurchase(consumeParams)
+        }
+        // [END android_playbilling_consume_purchase]
+    }
+
+    private suspend fun acknowledgePurchase(purchase: Purchase) {
+        // [START android_playbilling_acknowledge_purchase]
+        val client: BillingClient = billingClient
+        val acknowledgePurchaseResponseListener: AcknowledgePurchaseResponseListener = AcknowledgePurchaseResponseListener { }
+
+        val acknowledgePurchaseParams = AcknowledgePurchaseParams.newBuilder()
+            .setPurchaseToken(purchase.purchaseToken)
+        val ackPurchaseResult = withContext(Dispatchers.IO) {
+            client.acknowledgePurchase(acknowledgePurchaseParams.build())
+        }
+        // [END android_playbilling_acknowledge_purchase]
+    }
+
+    private fun getBillingConfig() {
+        // [START android_playbilling_get_billing_config]
+        // Use the default GetBillingConfigParams.
+        val getBillingConfigParams = GetBillingConfigParams.newBuilder().build()
+        billingClient.getBillingConfigAsync(
+            getBillingConfigParams,
+            object : BillingConfigResponseListener {
+                override fun onBillingConfigResponse(
+                    billingResult: BillingResult,
+                    billingConfig: BillingConfig?
+                ) {
+                    if (billingResult.responseCode == BillingResponseCode.OK &&
+                        billingConfig != null
+                    ) {
+                        val countryCode = billingConfig.countryCode
+                        // ...
+                    } else {
+                        // Handle errors
+                    }
+                }
+            }
+        )
+        // [END android_playbilling_get_billing_config]
+    }
+}
