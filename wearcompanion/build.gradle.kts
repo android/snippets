6e3754407a411513aae1674d85241fa6bd3bd4d7
Rebecca Franks <franks.rebecca@gmail.com>
2026-06-04 10:48:57 +0100

    fix build.

--- wearcompanion/build.gradle.kts
+++ wearcompanion/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.wear.snippets"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.wear.snippets"
