52970a91f11d6c3f7aae38843bf49caded1407b0
Paul Lammertsma <plammer@google.com>
2026-07-23 16:42:30 +0000

    [TV] Add OptIn(ExperimentalMaterial3Api::class) to MovieCard in TvMoviesActivity

--- tv/src/main/java/com/example/tv/ui/TvMoviesActivity.kt
+++ tv/src/main/java/com/example/tv/ui/TvMoviesActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -224,6 +225,7 @@ fun MovieCatalog(
 // [END android_compose_tv_lazyrow]
 
 // [START android_compose_tv_movie_card]
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieCard(
     movie: Movie,
