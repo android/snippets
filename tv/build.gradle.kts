48c004228e91f81861b0f6cd73594c2466815552
Paul Lammertsma <plammer@google.com>
2026-07-24 10:59:17 +0000

    Remove tv-foundation dependency in favor of standard Compose foundation

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -61,7 +61,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.tv.compose.material)
-    implementation(libs.tv.foundation)
     implementation(libs.coil.kt.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
