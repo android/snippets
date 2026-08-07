d9c3730a17393892cac556e263ffd44276d9a29b
bsagmoe <13055315+bsagmoe@users.noreply.github.com>
2026-08-07 14:03:43 +0000

    Apply Spotless

--- compose/snippets/src/main/java/com/example/compose/snippets/navigation3/migration/Navigator.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/navigation3/migration/Navigator.kt
@@ -24,9 +24,9 @@ import androidx.navigation3.runtime.NavKey
 /**
  * Handles navigation events (forward and back) by updating the navigation state.
  */
-class Navigator(val state: NavigationState){
-    fun navigate(route: NavKey){
-        if (route in state.backStacks.keys){
+class Navigator(val state: NavigationState) {
+    fun navigate(route: NavKey) {
+        if (route in state.backStacks.keys) {
             // This is a top level route, just switch to it.
             state.topLevelRoute = route
         } else {
@@ -34,13 +34,13 @@ class Navigator(val state: NavigationState){
         }
     }
 
-    fun goBack(){
-        val currentStack = state.backStacks[state.topLevelRoute] ?:
-        error("Stack for ${state.topLevelRoute} not found")
+    fun goBack() {
+        val currentStack = state.backStacks[state.topLevelRoute]
+            ?: error("Stack for ${state.topLevelRoute} not found")
         val currentRoute = currentStack.last()
 
         // If we're at the base of the current route, go back to the start route stack.
-        if (currentRoute == state.topLevelRoute){
+        if (currentRoute == state.topLevelRoute) {
             state.topLevelRoute = state.startRoute
         } else {
             currentStack.removeLastOrNull()
