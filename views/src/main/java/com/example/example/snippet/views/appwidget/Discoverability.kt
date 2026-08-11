196a76a0a4bcadb338af6a7377c7743ec176e13d
Katherine Kuan <843428+kkuan2011@users.noreply.github.com>
2026-08-11 00:17:31 +0000

    Add code snippets for appwidgets

---
+++ views/src/main/java/com/example/example/snippet/views/appwidget/Discoverability.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.example.snippet.views.appwidget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+
+private class ExampleAppWidgetProvider : AppWidgetProvider()
+
+private fun discoverabilitySnippet(context: Context) {
+    // [START android_views_appwidgets_discoverability_request_pin]
+    val appWidgetManager = AppWidgetManager.getInstance(context)
+    val myProvider = ComponentName(context, ExampleAppWidgetProvider::class.java)
+
+    if (appWidgetManager.isRequestPinAppWidgetSupported()) {
+        // Create the PendingIntent object only if your app needs to be notified
+        // when the user chooses to pin the widget. Note that if the pinning
+        // operation fails, your app isn't notified. This callback receives the ID
+        // of the newly pinned widget (EXTRA_APPWIDGET_ID).
+        val successCallback = PendingIntent.getBroadcast(
+            /* context = */ context,
+            /* requestCode = */ 0,
+            /* intent = */ Intent(
+                // [START_EXCLUDE silent]
+                    /*
+                    // [END_EXCLUDE]
+                    ...
+                    // [START_EXCLUDE silent]
+                     */
+                // [END_EXCLUDE]
+            ),
+            /* flags = */ PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        appWidgetManager.requestPinAppWidget(myProvider, null, successCallback)
+    }
+    // [END android_views_appwidgets_discoverability_request_pin]
+}
---
+++ views/src/main/java/com/example/example/snippet/views/appwidget/Enhance.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.example.snippet.views.appwidget
+
+import android.content.res.ColorStateList
+import android.util.TypedValue
+import android.widget.RemoteViews
+import com.example.example.snippet.views.R
+
+private fun createProgressColorStateList(): ColorStateList = ColorStateList.valueOf(0)
+
+private fun runtimeModRemoteViewsSnippet(remoteView: RemoteViews) {
+    // [START android_views_appwidgets_enhance_runtime_mod_remoteviews]
+    // Set the colors of a progress bar at runtime.
+    remoteView.setColorStateList(R.id.progress, "setProgressTintList", createProgressColorStateList())
+
+    // Specify exact sizes for margins.
+    remoteView.setViewLayoutMargin(R.id.text, RemoteViews.MARGIN_END, 8f, TypedValue.COMPLEX_UNIT_DIP)
+    // [END android_views_appwidgets_enhance_runtime_mod_remoteviews]
+}
