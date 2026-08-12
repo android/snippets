e0607f6071de14d01487312671f3b992d0caf1bd
morgillo <morgillo@google.com>
2026-08-12 12:07:51 +0200

    Remove the Fragment snippet as we only want to focus on Compose.
    Signed-off-by: morgillo <morgillo@google.com>

--- compose/snippets/src/main/java/com/example/compose/snippets/predictiveback/CustomBackSnippets.kt
+++ compose/snippets/src/main/java/com/example/compose/snippets/predictiveback/CustomBackSnippets.kt
@@ -16,32 +16,6 @@
 
 package com.example.compose.snippets.predictiveback
 
-import android.os.Bundle
-import androidx.activity.addCallback
-import androidx.fragment.app.Fragment
-
 private class CustomBackSnippets {
 
-    // [START android_custom_back_add_callback]
-    class MyFragment : Fragment() {
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-
-            // This callback will only be called when MyFragment is at least Started.
-            val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
-                // Handle the back button event
-            }
-
-            // The callback can be enabled or disabled here or in the lambda
-        }
-        // [START_EXCLUDE]
-        /*
-        // [END_EXCLUDE]
-        ...
-        // [START_EXCLUDE]
-        */
-        // [END_EXCLUDE]
-    }
-    // [END android_custom_back_add_callback]
 }
