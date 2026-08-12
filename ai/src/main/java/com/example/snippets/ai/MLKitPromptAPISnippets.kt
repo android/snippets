43241bd7a5e4886af2b1a573eb6b7db5430a6551
calren <5016806+calren@users.noreply.github.com>
2026-08-12 20:05:17 +0000

    Apply Spotless

--- ai/src/main/java/com/example/snippets/ai/MLKitPromptAPISnippets.kt
+++ ai/src/main/java/com/example/snippets/ai/MLKitPromptAPISnippets.kt
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.snippets.ai;
+
+package com.example.snippets.ai
 
 import com.google.mlkit.genai.prompt.GenerateContentRequest
 import com.google.mlkit.genai.prompt.Generation
@@ -36,7 +37,7 @@ fun instantiateModel() {
 
 class MLKitPromptAPISnippets {
     // [START android_snippets_structured_output_before]
-    suspend fun parseEmail(email:String): String {
+    suspend fun parseEmail(email: String): String {
         val parseEmailPrompt = "Parse this email and return the sender, title, and short summary of the email less than 10 words: "
 
         val parsedEmail = generativeModel.generateContent(parseEmailPrompt + email)
