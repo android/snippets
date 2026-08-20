6585c4282bc6f4db8d81eda2795a81bf7ebe1d95
acianrice <ciannosekai@gmail.com>
2026-08-20 14:41:45 -0500

    Revert "Adding input example project"
    This reverts commit 655767a11186309c5753aca252df04ccc8d8d091.

--- xr/src/main/java/com/example/xr/input/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
-}
