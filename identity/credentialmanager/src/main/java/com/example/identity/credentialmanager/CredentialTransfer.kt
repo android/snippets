a5697bfa5be8cd1dbb11f60502103665733c8027
vinishavathwani <35296563+vinishavathwani@users.noreply.github.com>
2026-08-25 06:03:17 +0000

    Apply Spotless

--- identity/credentialmanager/src/main/java/com/example/identity/credentialmanager/CredentialTransfer.kt
+++ identity/credentialmanager/src/main/java/com/example/identity/credentialmanager/CredentialTransfer.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -177,7 +177,6 @@ suspend fun startCredentialImport(
         // 2. Parse the FIDO CXF JSON string
         val cxfJsonString = response.response.responseJson
         parseAndSaveImportedCredentials(cxfJsonString)
-
     } catch (e: ImportCredentialsException) {
         // Handle specific import exceptions (e.g., ImportCredentialsCancellationException)
         handleImportFailure(e)
