e75676d0f13cb3a93b08b3fce53d4b8f239ead77
Barbara Law <barbara.law@stellarelements.com>
2026-09-01 15:09:53 -0700

    address pr feedback, move test file and clean up build file

--- backgroundwork/build.gradle.kts
+++ backgroundwork/build.gradle.kts
@@ -29,7 +29,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.androidx.work.runtime)
-    implementation("androidx.work:work-testing:2.10.0")
-    implementation("androidx.work:work-rxjava2:2.10.0")
-    implementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.work.testing)
+    androidTestImplementation(libs.androidx.work.rxjava2)
 }
