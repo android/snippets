ae59c8328ce2ca68ea7390a90cdd2ad6cb6b8fe5
Aaron Labiaga <alabiaga@google.com>
2026-09-02 13:35:36 -0600

    add subdirectory
    Change-Id: I0080011fab901337a14d06634bb3cf254dc7d968

--- security/src/main/java/com/example/snippets/security/window/SecureWindowActivity.kt
+++ security/src/main/java/com/example/snippets/security/window/SecureWindowActivity.kt
@@ -1,20 +1,4 @@
-/*
- * Copyright 2026 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.snippets.security
+package com.example.snippets.security.window
 
 import android.os.Bundle
 import android.view.WindowManager
@@ -26,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.example.snippets.security.R
 
 class SecureWindowActivity : ComponentActivity() {
 
